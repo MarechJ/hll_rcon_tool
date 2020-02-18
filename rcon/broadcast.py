8d613afd92533277d030dc7dab9f38425f5780ae
Julien marechal <julien.marechal@imc.com>
2020-02-18 18:25:46 +0100

    fix broadcast

--- rcon/broadcast.py
+++ rcon/broadcast.py
@@ -11,12 +11,13 @@ if __name__ == '__main__':
     mount_path = os.getenv("BROADCAST_PATH", '')
     
     with open(f"{mount_path}broadcasts.txt") as f: 
-        msgs = [l.split(' ', 1) for l in f] 
+        msgs = [l.split(' ', 1) for l in f]
+        msgs = [m for m in msgs if len(m) == 2] # filtering out badly formated lines
  
     while True: 
         for time_sec, msg in msgs:
             if msg == '/nextmap\n':
                 msg = "Next map: {}".format(ctl.get_next_map())
             print(f"{time_sec} second(s): {msg}")
-            #ctl.set_broadcast(msg) 
+            ctl.set_broadcast(msg) 
             time.sleep(int(time_sec)) 
