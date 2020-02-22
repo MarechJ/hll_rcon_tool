3d7a7ae9d9e344f029570b88056b7d1072868872
Julien marechal <julien.marechal@imc.com>
2020-02-22 13:57:12 +0100

    Added label to settings label slider

--- rcongui/src/components/SettingsView/hllSettings.js
+++ rcongui/src/components/SettingsView/hllSettings.js
@@ -1,5 +1,10 @@
 import React from "react";
 import { Grid, TextField, Slider, Typography } from "@material-ui/core";
+import { range } from "lodash/util";
+
+function valuetext(value) {
+  return `${value}`;
+}
 
 const NumSlider = ({
   classes,
@@ -7,6 +12,7 @@ const NumSlider = ({
   min = 0,
   max,
   value,
+  marks = true,
   step = 1,
   setValue
 }) => (
@@ -25,7 +31,7 @@ const NumSlider = ({
       onChange={(e, newVal) => setValue(newVal)}
       aria-labelledby="discrete-slider-always"
       step={step}
-      marks
+      marks={marks}
       min={min}
       max={max}
       valueLabelDisplay="auto"
@@ -61,41 +67,57 @@ class HLLSettings extends React.Component {
     return (
       <Grid container spacing={3} className={classes.paper}>
         <Grid item className={classes.paper} xs={12}>
+          <h1>This is not wire yet. Changing values won't have any effect</h1>
+        </Grid>
+        <Grid item className={classes.paper} xs={12}>
           <TextField
             fullWidth
             label="Server name"
             helperText="The server name as displayed in the server browser"
           />
         </Grid>
-        <Grid item className={classes.paper} xs={12} md={6} lg={4} xl={3}>
+        <Grid item className={classes.paper} xs={12} md={6}>
           <NumSlider
             classes={classes}
             text="Teamswitch cooldown (minutes)"
-            max={90}
+            max={100}
             value={teamSwitchCooldownMin}
+            marks={range(0, 120, 20).map(val => ({
+              value: val,
+              label: `${val}`
+            }))}
+            getAriaValueText={valuetext}
             setValue={val => this.setState({ teamSwitchCooldownMin: val })}
           />
         </Grid>
-        <Grid item className={classes.paper} xs={12} md={6} lg={4} xl={3}>
+        <Grid item className={classes.paper} xs={12} md={6}>
           <NumSlider
             classes={classes}
             text="Autobalance threshold"
             max={50}
             value={autoBalanceThres}
+            marks={range(0, 60, 10).map(val => ({
+              value: val,
+              label: `${val}`
+            }))}
             setValue={val => this.setState({ autoBalanceThres: val })}
           />
         </Grid>
-        <Grid item className={classes.paper} xs={12} md={6} lg={4} xl={3}>
+        <Grid item className={classes.paper} xs={12} md={6}>
           <NumSlider
             classes={classes}
             text="Idle autokick (minutes)"
             max={100}
             step={5}
+            marks={range(0, 120, 20).map(val => ({
+              value: val,
+              label: `${val}`
+            }))}
             value={idleAutokickMin}
             setValue={val => this.setState({ idleAutokickMin: val })}
           />
         </Grid>
-        <Grid item className={classes.paper} xs={12} md={6} lg={4} xl={3}>
+        <Grid item className={classes.paper} xs={12} md={6}>
           <NumSlider
             classes={classes}
             text="Maximum ping (ms)"
@@ -103,25 +125,34 @@ class HLLSettings extends React.Component {
             min={10}
             step={10}
             value={maxPingMs}
+            marks={range(0, 2500, 500).map(val => ({
+              value: val,
+              label: `${val}`
+            }))}
             setValue={val => this.setState({ maxPingMs: val })}
           />
         </Grid>
-        <Grid item className={classes.paper} xs={12} md={6} lg={4} xl={3}>
+        <Grid item className={classes.paper} xs={12} md={6}>
           <NumSlider
             classes={classes}
             text="Max queue length"
             max={5}
             min={1}
             value={queueLength}
+            marks={range(0, 6, 1).map(val => ({ value: val, label: `${val}` }))}
             setValue={val => this.setState({ queueLength: val })}
           />
         </Grid>
-        <Grid item className={classes.paper} xs={12} md={6} lg={4} xl={3}>
+        <Grid item className={classes.paper} xs={12} md={6}>
           <NumSlider
             classes={classes}
             text="Vip slots"
             max={100}
             value={vipSlots}
+            marks={range(0, 120, 20).map(val => ({
+              value: val,
+              label: `${val}`
+            }))}
             setValue={val => this.setState({ vipSlots: val })}
           />
         </Grid>
