03c6669a5ee48e4c35daefa0efb8f4fb7903b4c9
Alex McDonald <tanjitsu@hotmail.com>
2020-03-28 20:28:45 +0000

    Alter broadcast to only run if file exists. Added randomize_broadcast_available to flag broadcast is running. Added randomize_broadcast to collect if message are random.

--- rcon/broadcast.py
+++ rcon/broadcast.py
@@ -4,6 +4,7 @@ import random
 
 from rcon.extended_commands import Rcon
 from rcon.settings import SERVER_INFO
+from rcon.models import init_db, enter_session
 from rcon.user_config import get_user_config, set_user_config
 
 if __name__ == '__main__':
@@ -11,20 +12,23 @@ if __name__ == '__main__':
         SERVER_INFO
     )
     mount_path = os.getenv("BROADCAST_PATH", '')
+
+    set_user_config('randomize_broadcast_available', False)
     
-    with open(f"{mount_path}broadcasts.txt") as f: 
-        msgs = [l.split(' ', 1) for l in f]
-        msgs = [m for m in msgs if len(m) == 2] # filtering out badly formated lines
+    if os.path.isfile(f"{mount_path}broadcasts.txt"):
+        with open(f"{mount_path}broadcasts.txt") as f: 
+            msgs = [l.split(' ', 1) for l in f]
+            msgs = [m for m in msgs if len(m) == 2] # filtering out badly formated lines
 
-    if (msgs.length):
-        set_user_config('randomize_broadcast_active', True)
- 
-    while True: 
-        if get_user_config('randomize_broadcast'):
-            random.shuffle(msgs)
+        if type(msgs) is list:
+            set_user_config('randomize_broadcast_available', True)
 
-        for time_sec, msg in msgs:
-            msg = msg.rstrip("\r\n").format(nextmap=ctl.get_next_map())
-            print(f"{time_sec} second(s): {msg}")
-            ctl.set_broadcast(msg) 
-            time.sleep(int(time_sec)) 
+            while True: 
+                if get_user_config('randomize_broadcast'):
+                    random.shuffle(msgs)
+
+                for time_sec, msg in msgs:
+                    msg = msg.rstrip("\r\n").format(nextmap=ctl.get_next_map())
+                    print(f"{time_sec} second(s): {msg}")
+                    ctl.set_broadcast(msg) 
+                    time.sleep(int(time_sec)) 
