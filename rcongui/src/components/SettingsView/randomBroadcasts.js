cf1204f71461fb91ba13512a98626fc8a00811a9
Alex McDonald <tanjitsu@hotmail.com>
2020-03-28 20:29:35 +0000

    Updated randomBroadcasts button.

--- rcongui/src/components/SettingsView/randomBroadcasts.js
+++ rcongui/src/components/SettingsView/randomBroadcasts.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import { toast } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css";
+import { showResponse } from "../../utils/fetchUtils";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import Grid from "@material-ui/core/Grid";
 
-let active = true;
+let active = false;
 
-fetch(`${process.env.REACT_APP_API_URL}get_randomMessagesActive`)
-        .then(data => console.log("Messges Active: " + data))
-        .catch(error => toast.error("Unable to connect to API " + error));
+fetch(`${process.env.REACT_APP_API_URL}get_randomMessagesAvailable`)
+  .then(response => showResponse(response, "get_randomMessagesAvailable"))
+    .then(data => active = data.result)
+      .catch(error => toast.error("Unable to connect to API " + error));
+
+const button = {
+  "values": [
+        {
+            name: "Random Order",
+            value: "true"
+        },
+        {
+            name: "Sequential Order",
+            value: "false"
+        }
+    ]
+};
 
 const Selector = ({
     classes,
@@ -45,37 +60,38 @@ const Selector = ({
 
 class RandomBroadcast extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {
+          status: false
+        };
+    }
+
+    componentDidMount() {
+      this.load()
     }
 
     async load(command) {
         return fetch(`${process.env.REACT_APP_API_URL}get_randomMessages`)
-            .then(data => console.log("Random Messges: " + data))
+          .then(response => showResponse(response, command))
+            .then(data => this.state.status = data.result)
             .catch(error => toast.error("Unable to connect to API " + error));
+    }
+
+    handler(value)
+    {
+      this.setState({ current: value })
+
+      this.props.broadcastChange(value)
     }
 
     render() {
         if (!active) return null;
 
-        const { classes, width } = this.props;
-  
-        const button = {
-            "values": [
-                {
-                    name: "Random Order",
-                    value: "on"
-                },
-                {
-                    name: "Sequential Order",
-                    value: "off"
-                }
-            ]
-        };
+        const {
+          status
+        } = this.state;
 
-        let current = button.values[0].value;
-        
-        if (!this.data)
-            current = button.values[1].value;
+        const { classes, width } = this.props;
 
         return (
             <React.Fragment>
@@ -83,11 +99,11 @@ class RandomBroadcast extends React.Component {
                     <Selector
                         classes={classes}
                         values={button.values}
-                        onChange={value =>
-                            this.props.broadcastChange(value)
+                        onChange={value => 
+                          this.handler(value)
                         }
-                        currentValue={current}
-                        kind="Toggle Broadcast message order"
+                        currentValue={status}
+                        kind="Broadcast Messages are running in "
                     />
                 </Grid>
             </React.Fragment>
