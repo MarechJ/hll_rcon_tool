ed4eb921d54a9f75a57cda05ddaa8af06f271218
Julien Marechal <julien.marechal35@gmail.com>
2020-03-29 00:20:34 +0100

    Adding missing component. #patch

---
+++ rcongui/src/components/SettingsView/padlock.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
+export default function Padlock({handleChange, checked, label}) {
+
+  return (
+    <FormGroup row>
+      <FormControlLabel
+        control={
+          <Switch
+            checked={checked}
+            onChange={handleChange}
+            name={label}
+            color="primary"
+          />
+        }
+        label={label}
+      />
+    </FormGroup>
+  );
+}
