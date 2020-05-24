c66b2dfc9b802067bba81cc1cefc0a2d9e93d099
Julien Marechal <julien.marechal35@gmail.com>
2020-05-24 15:57:52 +0200

    removing conflict

--- rconweb/rconweb/settings.py
+++ rconweb/rconweb/settings.py
@@ -225,5 +225,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = '/static/'
+STATIC_URL = '/djangostatic/'
+STATIC_ROOT = '/djangostatic/'
