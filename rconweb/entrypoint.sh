bd1d54ca492ca14725497c28bf5ea8e120158db8
Julien Marechal <julien.marechal35@gmail.com>
2020-05-24 15:49:07 +0200

    testing static files handling

--- rconweb/entrypoint.sh
+++ rconweb/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations --no-input
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('admin', 'admin@myproject.com', 'admin')" | python manage.py shell
 gunicorn -w 4 -b 0.0.0.0 rconweb.wsgi
