e5cfd474e892efd580d5922b88767cd51146e19b
Tuomo <tuokri@tuta.io>
2020-06-13 17:00:38 +0300

    More error handling and logging in Discord chat webhook

--- rcon/discord_chat.py
+++ rcon/discord_chat.py
@@ -9,13 +9,18 @@ from discord import Webhook
 from rcon.game_logs import on_chat
 
 DISCORD_CHAT_WEBHOOK_URL = os.getenv("DISCORD_CHAT_WEBHOOK")
-WEBHOOK_ID = None
-WEBHOOK_TOKEN = None
 
 logger = logging.getLogger(__name__)
 
 
 def post_chat_message_to_discord(_, log):
+    if not DISCORD_CHAT_WEBHOOK_URL:
+        return
+    if not WEBHOOK_ID:
+        return
+    if not WEBHOOK_TOKEN:
+        return
+
     try:
         message = log["message"]
         message = discord.utils.escape_mentions(message)
@@ -24,6 +29,7 @@ def post_chat_message_to_discord(_, log):
         webhook = Webhook.partial(
             id=WEBHOOK_ID, token=WEBHOOK_TOKEN, adapter=RequestsWebhookAdapter()
         )
+        logger.info("sending chat message len=%s to Discord", len(message))
         webhook.send(message)
 
     except Exception as e:
@@ -35,7 +41,6 @@ def parse_webhook_url(url):
     by performing a get request and checking existence
     of 'id' and 'token' in the JSON response.
     """
-
     try:
         resp = requests.get(url).json()
         _id = int(resp["id"])
@@ -43,9 +48,11 @@ def parse_webhook_url(url):
         return _id, token
     except Exception as e:
         logger.error("error parsing Discord chat webhook url: %s", e)
+        return None, None
 
 
 # Only parse ID and token and apply decorator if webhook URL was provided.
 if DISCORD_CHAT_WEBHOOK_URL:
     WEBHOOK_ID, WEBHOOK_TOKEN = parse_webhook_url(DISCORD_CHAT_WEBHOOK_URL)
     post_chat_message_to_discord = on_chat(post_chat_message_to_discord)
+    logger.info("Discord chat webhook initialized")
