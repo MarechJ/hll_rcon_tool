5bcce35bb236d2088e90c94ea1fcb09e3152bba2
Tuomo <tuokri@tuta.io>
2020-06-16 10:16:26 +0300

    Review improvements

--- rcon/discord_chat.py
+++ rcon/discord_chat.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from functools import lru_cache
 
 import discord.utils
 import requests
@@ -13,12 +14,10 @@ DISCORD_CHAT_WEBHOOK_URL = os.getenv("DISCORD_CHAT_WEBHOOK")
 logger = logging.getLogger(__name__)
 
 
+@on_chat
 def post_chat_message_to_discord(_, log):
-    if not DISCORD_CHAT_WEBHOOK_URL:
-        return
-    if not WEBHOOK_ID:
-        return
-    if not WEBHOOK_TOKEN:
+    webhook_id, webhook_token = parse_webhook_url(DISCORD_CHAT_WEBHOOK_URL)
+    if not all([webhook_id, webhook_token]):
         return
 
     try:
@@ -27,7 +26,7 @@ def post_chat_message_to_discord(_, log):
         message = discord.utils.escape_markdown(message)
 
         webhook = Webhook.partial(
-            id=WEBHOOK_ID, token=WEBHOOK_TOKEN, adapter=RequestsWebhookAdapter()
+            id=webhook_id, token=webhook_token, adapter=RequestsWebhookAdapter()
         )
         logger.info("sending chat message len=%s to Discord", len(message))
         webhook.send(message)
@@ -36,11 +35,16 @@ def post_chat_message_to_discord(_, log):
         logger.error("error executing chat message webhook: %s", e)
 
 
+@lru_cache
 def parse_webhook_url(url):
     """Parse and check validity of Discord webhook URL
     by performing a get request and checking existence
     of 'id' and 'token' in the JSON response.
     """
+    if not url:
+        logger.info("no Discord chat webhook URL provided")
+        return None, None
+
     try:
         resp = requests.get(url).json()
         _id = int(resp["id"])
@@ -49,10 +53,3 @@ def parse_webhook_url(url):
     except Exception as e:
         logger.error("error parsing Discord chat webhook url: %s", e)
         return None, None
-
-
-# Only parse ID and token and apply decorator if webhook URL was provided.
-if DISCORD_CHAT_WEBHOOK_URL:
-    WEBHOOK_ID, WEBHOOK_TOKEN = parse_webhook_url(DISCORD_CHAT_WEBHOOK_URL)
-    post_chat_message_to_discord = on_chat(post_chat_message_to_discord)
-    logger.info("Discord chat webhook initialized")
