b3221400f4499fd55b4f48599d1addc018090bb3
Julien Marechal <julien.marechal35@gmail.com>
2020-08-01 19:45:15 +0200

    added tools for votemap

--- rcon/broadcast.py
+++ rcon/broadcast.py
@@ -8,11 +8,19 @@ import json
 import redis
 
 from rcon.cache_utils import get_redis_pool
-from rcon.extended_commands import Rcon
+from rcon.extended_commands import Rcon, CommandFailedError
 from rcon.settings import SERVER_INFO
 from rcon.user_config import AutoBroadcasts
 from rcon.audit import online_mods, get_registered_mods
-from rcon.utils import HUMAN_MAP_NAMES, number_to_map
+from rcon.utils import (
+    LONG_HUMAN_MAP_NAMES, 
+    SHORT_HUMAN_MAP_NAMES, 
+    NO_MOD_LONG_HUMAN_MAP_NAMES, 
+    NO_MOD_SHORT_HUMAN_MAP_NAMES, 
+    numbered_maps, 
+    categorize_maps, 
+    get_current_selection 
+)
 from functools import wraps
 
 
@@ -30,7 +38,10 @@ def get_votes_status():
 def format_winning_map(winning_maps, display_count=2, default=''):
     if not winning_maps:
         return f'Defaulting to: {default}'
-    return ', '.join(f'{HUMAN_MAP_NAMES[m]} ({count} vote(s))' for m, count in winning_maps[:display_count])
+    wins = winning_maps[:display_count]
+    if display_count == 0:
+        wins = winning_maps
+    return ', '.join(f'{LONG_HUMAN_MAP_NAMES[m]} ({count} vote(s))' for m, count in wins)
 
 logger = logging.getLogger(__name__)
 
@@ -54,6 +65,21 @@ def chunks(lst, n):
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
 
+def scrolling_votemap(rcon, winning_maps, repeat=10):
+    vote_options = format_map_vote(rcon, "line", short_names=False)
+    separator = '    *    '
+    options = separator.join([vote_options] * repeat)
+    instructions = os.getenv('VOTE_MAP_INSTRUCTIONS', 'To vote write the map number in the chat')
+    repeat_instructions = max(int(len(options) / (len(instructions) + len(separator))), 1)
+    instructions = separator.join([instructions] * repeat_instructions)
+
+    winning_maps = format_winning_map(winning_maps, display_count=0, default='No votes recorded')
+    repeat_winning_maps = max(int(len(options) / (len(winning_maps) + len(separator))), 1)
+    winning_maps = separator.join([winning_maps] * repeat_winning_maps)
+
+    return "{}\n{}\n{}".format(options, instructions, winning_maps)
+
+
 def format_by_line_length(possible_votes, max_length=50):
     """
     Note: I've tried to format with a nice aligned table but it's not
@@ -70,11 +96,40 @@ def format_by_line_length(possible_votes, max_length=50):
     lines.append(line)       
     return "\n".join(lines)                                                                                                                                                                                                                                                                             
 
+def join_vote_options(join_char, selection, human_name_map, maps_to_numbers):
+    return join_char.join(f"[{maps_to_numbers[m]}] {human_name_map[m]}" for m in selection)
 
-def format_map_vote(rcon):
-    vote_dict = number_to_map(rcon)
-    items = [f"[{k}] {HUMAN_MAP_NAMES.get(v, v)}"  for k, v in vote_dict.items()]
-    return format_by_line_length(items)
+def format_map_vote(rcon, format_type="line", short_names=True):
+    selection = get_current_selection()
+    if not selection:
+        return "No votes possible"
+    human_map = SHORT_HUMAN_MAP_NAMES if short_names else LONG_HUMAN_MAP_NAMES
+    human_map_mod = NO_MOD_SHORT_HUMAN_MAP_NAMES if short_names else NO_MOD_LONG_HUMAN_MAP_NAMES
+    vote_dict = numbered_maps(selection)
+    maps_to_numbers = dict(zip(vote_dict.values(), vote_dict.keys()))
+    items = [f"[{k}] {human_map.get(v, v)}"  for k, v in vote_dict.items()]
+    if format_type == "line":
+        return ' '.join(items)
+    if format_type == "max_length":
+        return format_by_line_length(items)
+    if format_type == 'vertical':
+        return '\n'.join(items)
+    if format_type.startswith('by_mod'):
+        categorized = categorize_maps(selection)
+        off = join_vote_options(' ', categorized['offensive'], human_map_mod, maps_to_numbers)
+        warfare = join_vote_options(' ', categorized['warfare'], human_map_mod, maps_to_numbers)
+        if format_type == 'by_mod_line':
+            return "OFFENSIVE: {} WARFARE: {}".format(off, warfare)
+        if format_type == 'by_mod_vertical':
+            return "OFFENSIVE:\n{}\nWARFARE:\n{}".format(off, warfare)
+        if format_type == 'by_mod_split':
+            return "OFFENSIVE: {}\nWARFARE: {}".format(off, warfare)
+        if format_type == 'by_mod_vertical_all':
+            return "OFFENSIVE:\n{}\nWARFARE:\n{}".format(
+                join_vote_options('\n', categorized['offensive'], human_map_mod, maps_to_numbers),
+                join_vote_options('\n', categorized['warfare'], human_map_mod, maps_to_numbers)
+            )
+    
     
 
 def format_message(ctl, msg):
@@ -97,11 +152,23 @@ def format_message(ctl, msg):
         'juniors': ','.join(safe(get_junior_names, [])()),
         'vips': ', '.join(safe(get_vip_names, [])()),
         'randomvip': safe(lambda: random.choice(get_vip_names() or [""]), "")(),
-        'votenextmap': safe(format_map_vote, '')(ctl),
+        'votenextmap_line': safe(partial(format_map_vote, format_type='line'), '')(ctl),
+        'votenextmap_noscroll': safe(partial(format_map_vote, format_type='max_length'), '')(ctl),
+        'votenextmap_vertical': safe(partial(format_map_vote, format_type='vertical'), '')(ctl),
+        'votenextmap_by_mod_line': safe(partial(format_map_vote, format_type='by_mod_line'), '')(ctl),
+        'votenextmap_by_mod_vertical': safe(partial(format_map_vote, format_type='by_mod_vertical'), '')(ctl),
+        'votenextmap_by_mod_vertical_all': safe(partial(format_map_vote, format_type='by_mod_vertical_all'), '')(ctl),
+        'votenextmap_by_mod_split': safe(partial(format_map_vote, format_type='by_mod_split'), '')(ctl),
         'total_votes': vote_status['total_votes'],
-        'winning_maps': format_winning_map(vote_status['winning_maps'], default=nextmap)
+        'winning_maps_short': format_winning_map(vote_status['winning_maps'], default=nextmap, display_count=2),
+        'winning_maps_all': format_winning_map(vote_status['winning_maps'], default=nextmap, display_count=0),
+        'scrolling_votemap': scrolling_votemap(ctl, vote_status['winning_maps'])
     }
-    return msg.format(**subs)
+    try: 
+        return msg.format(**subs)
+    except KeyError as e:
+        logger.warning("Can't broadcast message correctly, variable does not exists %s", e)
+        return msg
 
 def run():
     # avoid circular import
@@ -131,7 +198,10 @@ def run():
             
             formatted = format_message(ctl, msg)
             logger.debug("Broadcasting for %s seconds: %s", time_sec, formatted)
-            ctl.set_broadcast(formatted) 
+            try:
+                ctl.set_broadcast(formatted) 
+            except CommandFailedError:
+                logger.exception("Unable to broadcast %s", msg)
             time.sleep(int(time_sec)) 
 
 
--- rcon/utils.py
+++ rcon/utils.py
@@ -1,3 +1,10 @@
+import json
+import redis
+import logging
+from rcon.cache_utils import get_redis_pool
+
+logger = logging.getLogger('rcon')
+
 def get_current_map(rcon):
     map_ = rcon.get_map()
 
@@ -6,25 +13,60 @@ def get_current_map(rcon):
 
     return map_
 
-def number_to_map(rcon):
-    mapping = {}
-    current = get_current_map(rcon)
+def get_current_selection():
+    red = redis.StrictRedis(connection_pool=get_redis_pool())
+    selection = red.get("votemap_selection")
+    if selection:
+        selection = json.loads(selection)
+    return selection
 
-    for idx, map_ in enumerate(sorted(rcon.get_maps())):
-        if map_ == current:
-            idx = 'x'
-        mapping[str(idx)] = map_
-    
-    return mapping
+def numbered_maps(maps):
+    return  {
+        str(idx): map_
+        for idx, map_ in enumerate(maps)
+    }
+   
+def categorize_maps(maps):
+    warfare_offsensive = {
+        'warfare': [],
+        'offensive': [],
+    }
+    for m in maps:
+        if 'offensive' in m:
+            warfare_offsensive['offensive'].append(m)
+        if 'warfare' in m:
+            warfare_offsensive['warfare'].append(m)
 
+    return warfare_offsensive
 
+def map_name(map_):
+    name, *rest = map_.split('_')
+    return name
 
-HUMAN_MAP_NAMES = {
+LONG_HUMAN_MAP_NAMES = {
+    "foy_warfare": "Foy",
+    "stmariedumont_warfare": "St Marie du Mont",
+    "hurtgenforest_warfare": "Hurtgen forest", 
+    "utahbeach_warfare": "Utah beach", 
+    "omahabeach_offensive_us": "Offensive Omaha beach", 
+    "stmereeglise_warfare": "St Mere Eglise",
+    "stmereeglise_offensive_ger": "Offensive St Mere eglise (Ger)", 
+    "foy_offensive_ger": "Offensive Foy", 
+    "purpleheartlane_warfare": "Purple Heart Lane",
+    "purpleheartlane_offensive_us": "Offensive Purple Heart Lane",
+    "hill400_warfare": "Hill 400",
+    "hill400_offensive_US": "Offensive Hill 400",
+    "stmereeglise_offensive_us": "Offensive St Mere Eglise (US)",
+    "carentan_warfare": "Carentan",
+    "carentan_offensive_us": "Offensive Carentan"
+}
+
+SHORT_HUMAN_MAP_NAMES = {
     "foy_warfare": "Foy",
     "stmariedumont_warfare": "St.Marie",
     "hurtgenforest_warfare": "Hurtgen", 
     "utahbeach_warfare": "Utah", 
-    "omahabeach_offensive_us": "Omaha", 
+    "omahabeach_offensive_us": "Off. Omaha", 
     "stmereeglise_warfare": "SME",
     "stmereeglise_offensive_ger": "Off. SME(Ger)", 
     "foy_offensive_ger": "Off. Foy", 
@@ -36,3 +78,77 @@ HUMAN_MAP_NAMES = {
     "carentan_warfare": "Carentan",
     "carentan_offensive_us": "Off. Carentan"
 }
+
+
+NO_MOD_LONG_HUMAN_MAP_NAMES = {
+    "foy_warfare": "Foy",
+    "stmariedumont_warfare": "St Marie du Mont",
+    "hurtgenforest_warfare": "Hurtgen orest", 
+    "utahbeach_warfare": "Utah beach", 
+    "omahabeach_offensive_us": "Omaha beach", 
+    "stmereeglise_warfare": "St Mere Eglise",
+    "stmereeglise_offensive_ger": "St Mere eglise (Ger)", 
+    "foy_offensive_ger": "Foy", 
+    "purpleheartlane_warfare": "Purple Heart Lane",
+    "purpleheartlane_offensive_us": "Purple Heart Lane",
+    "hill400_warfare": "Hill 400",
+    "hill400_offensive_US": "Hill 400",
+    "stmereeglise_offensive_us": "St Mere Eglise (US)",
+    "carentan_warfare": "Carentan",
+    "carentan_offensive_us": "Carentan"
+}
+
+NO_MOD_SHORT_HUMAN_MAP_NAMES = {
+    "foy_warfare": "Foy",
+    "stmariedumont_warfare": "St.Marie",
+    "hurtgenforest_warfare": "Hurtgen", 
+    "utahbeach_warfare": "Utah", 
+    "omahabeach_offensive_us": "Omaha", 
+    "stmereeglise_warfare": "SME",
+    "stmereeglise_offensive_ger": "SME(Ger)", 
+    "foy_offensive_ger": "Foy", 
+    "purpleheartlane_warfare": "PHL",
+    "purpleheartlane_offensive_us": "PHL",
+    "hill400_warfare": "Hill400",
+    "hill400_offensive_US": "Hill400",
+    "stmereeglise_offensive_us": "SME (US)",
+    "carentan_warfare": "Carentan",
+    "carentan_offensive_us": "Carentan"
+}
+
+
+class FixedLenList:
+    def __init__(self, key, max_len=100, serializer=json.dumps, deserializer=json.loads):
+        self.red = redis.StrictRedis(connection_pool=get_redis_pool())
+        self.max_len = max_len
+        self.serializer = serializer
+        self.deserializer = deserializer
+        self.key = key
+
+    def add(self, obj):
+        self.red.lpush(self.key, self.serializer(obj))
+        self.red.ltrim(self.key, 0, self.max_len - 1)
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            if index.step:
+                raise ValueError("Step is supported")
+            end = index.stop or -1
+            start = index.start or 0
+            return [
+                self.deserializer(o) 
+                for o in self.red.lrange(self.key, start, end)
+            ]
+        val = self.red.lindex(self.key, index)
+        if val is None:
+            raise IndexError("Index out of bound")
+        return self.deserializer(val)
+
+    def lpop(self):
+        val = self.red.lpop(self.key)
+        if val is None:
+            return val
+        return self.deserializer(val)
+
+    def lpush(self, obj):
+        self.red.lpush(self.key, self.serializer(obj))
