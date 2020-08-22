52b0ee1b00ed6f394e1a2e6e652f6c0c5d146ca0
Julien Marechal <julien.marechal35@gmail.com>
2020-08-22 12:40:05 +0200

    hotfix for super user not being created

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ then
       exit 0
   fi
   cd rconweb 
-  ./manage.py migrate
+  ./manage.py migrate --noinput
   ./manage.py collectstatic --noinput
   echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin') if not User.objects.filter(username='admin').first() else None" | python manage.py shell
   gunicorn -w 8 -k eventlet -t 120 -b 0.0.0.0 rconweb.wsgi
