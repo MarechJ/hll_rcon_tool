8d2fd908d87fe63753cbb9764ac14ca46ac38c16
Alex McDonald <tanjitsu@hotmail.com>
2020-10-31 12:31:07 +0000

    Hardcoded Scure URL.

--- rcongui/src/components/PlayerView/playerList.js
+++ rcongui/src/components/PlayerView/playerList.js
@@ -124,7 +124,7 @@ const getCountry = (player) => {
       alt={country}
       title={country ? getName(country) : ""}
       style={{ height: "12px" }}
-      src={ window.location.protocol + `//catamphetamine.gitlab.io/country-flag-icons/3x2/${country}.svg`}
+      src={ `https://catamphetamine.gitlab.io/country-flag-icons/3x2/${country}.svg`}
     />
   );
 };
@@ -192,7 +192,7 @@ const PlayerItem = ({
               className={classes.marginRight}
               target="_blank"
               color="inherit"
-              href={ window.location.protocol + `//steamcommunity.com/profiles/${steamID64}`}
+              href={ `https://steamcommunity.com/profiles/${steamID64}`}
             >
               {player.get("is_vip") ? (
                 <React.Fragment>
