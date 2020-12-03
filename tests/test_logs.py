be0a501da188ee9b467aca79e4b70c90fa9bfec4
Julien marechal <julien.marechal@imc.com>
2020-12-03 17:19:55 +0100

    added parsin for votes

--- tests/test_logs.py
+++ tests/test_logs.py
@@ -29,25 +29,153 @@ RAW_VOTE = """
 [5:11 min (1606999065)] VOTE Vote Kick {Jesse Pingman} successfully passed. [For: 2/0 - Against: 0]
 """
 
-@mock.patch('rcon.extended_commands.ServerCtl._connect', side_effect=lambda *args: print("Connecting"))
+
+@mock.patch(
+    "rcon.extended_commands.ServerCtl._connect",
+    side_effect=lambda *args: print("Connecting"),
+)
 def test_chat(*mocks):
-    res = Rcon.parse_logs("[16:56 min (1606998360)] CHAT[Team][Zuiho(Allies/76561198088159692)]: thx")
+    res = Rcon.parse_logs(
+        "[16:56 min (1606998360)] CHAT[Team][Zuiho(Allies/76561198088159692)]: thx"
+    )
 
-    assert res['logs'][0]['player'] == 'Zuiho' 
-    assert res['logs'][0]['sub_content'] == 'thx'
-    assert res['logs'][0]['steam_id_64_1'] == '76561198088159692'
-    assert res['logs'][0]['action'] == 'CHAT[Allies][Team]'
-    assert res['logs'][0]['timestamp_ms'] == 1606998360000
+    assert res["logs"][0]["player"] == "Zuiho"
+    assert res["logs"][0]["sub_content"] == "thx"
+    assert res["logs"][0]["steam_id_64_1"] == "76561198088159692"
+    assert res["logs"][0]["action"] == "CHAT[Allies][Team]"
+    assert res["logs"][0]["timestamp_ms"] == 1606998360000
 
 
-@mock.patch('rcon.extended_commands.ServerCtl._connect', side_effect=lambda *args: print("Connecting"))
+@mock.patch(
+    "rcon.extended_commands.ServerCtl._connect",
+    side_effect=lambda *args: print("Connecting"),
+)
+def test_vote_started(*mocks):
+    res = Rcon.parse_logs(
+        "[15:49 min (1606998428)] VOTE Player [[fr]ELsass_blitz] Started a vote of type (PVR_Kick_Abuse) against [拢儿]. VoteID: [1]"
+    )
+
+    assert res["logs"][0]["player"] == "[fr]ELsass_blitz"
+    assert res["logs"][0]["player2"] == "拢儿"
+    # assert res['logs'][0]['sub_content'] == 'thx'
+    assert res["logs"][0]["steam_id_64_1"] == None
+    assert res["logs"][0]["steam_id_64_2"] == None
+    assert res["logs"][0]["action"] == "VOTE STARTED"
+    assert res["logs"][0]["timestamp_ms"] == 1606998428000
+
+
+@mock.patch(
+    "rcon.extended_commands.ServerCtl._connect",
+    side_effect=lambda *args: print("Connecting"),
+)
+def test_vote_completed(*mocks):
+    res = Rcon.parse_logs(
+        "[5:11 min (1606999065)] VOTE Vote [3] completed. Result: PVR_Passed"
+    )
+
+    assert res["logs"][0]["player"] == None
+    assert res["logs"][0]["player2"] == None
+    # assert res['logs'][0]['sub_content'] == 'thx'
+    assert res["logs"][0]["steam_id_64_1"] == None
+    assert res["logs"][0]["steam_id_64_2"] == None
+    assert res["logs"][0]["action"] == "VOTE COMPLETED"
+
+    res = Rcon.parse_logs(
+        "[5:11 min (1606999065)] VOTE Vote Kick {Jesse Pingman} successfully passed. [For: 2/0 - Against: 0]"
+    )
+
+    assert res["logs"][0]["player"] == "Jesse Pingman"
+    assert res["logs"][0]["player2"] == None
+    # assert res['logs'][0]['sub_content'] == 'thx'
+    assert res["logs"][0]["steam_id_64_1"] == None
+    assert res["logs"][0]["steam_id_64_2"] == None
+    assert res["logs"][0]["action"] == "VOTE COMPLETED"
+
+
+@mock.patch(
+    "rcon.extended_commands.ServerCtl._connect",
+    side_effect=lambda *args: print("Connecting"),
+)
+def test_vote(*mocks):
+    res = Rcon.parse_logs(
+        "[13:19 min (1606998577)] VOTE Player [nsancho] voted [PV_Favour] for VoteID[2]"
+    )
+
+    assert res["logs"][0]["player"] == "nsancho"
+    assert res["logs"][0]["player2"] == None
+    # assert res['logs'][0]['sub_content'] == 'thx'
+    assert res["logs"][0]["steam_id_64_1"] == None
+    assert res["logs"][0]["steam_id_64_2"] == None
+    assert res["logs"][0]["action"] == "VOTE"
+
+
+@mock.patch(
+    "rcon.extended_commands.ServerCtl._connect",
+    side_effect=lambda *args: print("Connecting"),
+)
 def test_log_parsing(*mocks):
-    with mock.patch('rcon.extended_commands.ServerCtl.get_logs', return_value=RAW_LOGS):
+    with mock.patch(
+        "rcon.extended_commands.ServerCtl.get_logs",
+        return_value=RAW_LOGS + "\n" + RAW_VOTE,
+    ):
         res = Rcon({}).get_structured_logs(30)
 
-        assert set(res['actions']) == set(['DISCONNECTED', 'CHAT[Allies]', 'CHAT[Axis]', 'CHAT[Allies][Unit]', 'KILL', 'CONNECTED', 'CHAT[Allies][Team]', 'CHAT[Axis][Team]', 'CHAT[Axis][Unit]', 'CHAT', "TEAM KILL"])
-        assert set(res['players']) == set([None, '[CPC] xALF', '湊あくあ', 'Karadoc', 'Dieter Schlüter: b', 'Karadoc', 'Waxxeer', 'Bullitt-FR', 'fguitou', 'bananacocoo', 'bananacocoo : toto', '[bananacocoo', ']bananacocoo'])
-        assert set(l['timestamp_ms'] for l in res['logs']) == {1606340677000, 1606340690000}
+        assert set(res["actions"]) == set(
+            [
+                "DISCONNECTED",
+                "CHAT[Allies]",
+                "CHAT[Axis]",
+                "CHAT[Allies][Unit]",
+                "KILL",
+                "CONNECTED",
+                "CHAT[Allies][Team]",
+                "CHAT[Axis][Team]",
+                "CHAT[Axis][Unit]",
+                "CHAT",
+                "VOTE COMPLETED",
+                "VOTE STARTED",
+                "VOTE",
+            ]
+        )
+        assert set(res["players"]) == set(
+            [
+                None,
+                "[CPC] xALF",
+                "湊あくあ",
+                "Karadoc",
+                "Dieter Schlüter: b",
+                "Karadoc",
+                "Waxxeer",
+                "Bullitt-FR",
+                "fguitou",
+                "bananacocoo",
+                "bananacocoo : toto",
+                "[bananacocoo",
+                "Codyckj",
+                "nsancho",
+                "拢儿",
+                "]bananacocoo",
+                "[ARC] MaTej ★ツ",
+                "Cpt.Lati",
+                "[fr]ELsass_blitz",
+                "Galiat",
+                "[TGF] AstroHeap",
+                "Jesse Pingman",
+            ]
+        )
+        assert set(l["timestamp_ms"] for l in res["logs"]) == {
+            1606999060000,
+            1606340677000,
+            1606340690000,
+            1606998428000,
+            1606998577000,
+            1606998429000,
+            1606999065000,
+            1606998582000,
+            1606999058000,
+            1606998434000,
+            1606998575000,
+        }
 
-        res = Rcon({}).get_structured_logs(30, filter_action='CHAT')
-        assert all('CHAT' in l['action'] for l in res['logs'])
+        res = Rcon({}).get_structured_logs(30, filter_action="CHAT")
+        assert all("CHAT" in l["action"] for l in res["logs"])
