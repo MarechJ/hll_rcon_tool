aaf14391218b13f4dd983c58adab127d4f993106
Julien Marechal <julien.marechal35@gmail.com>
2021-04-01 00:29:07 +0200

    fix map recorder

--- rcon/map_recorder.py
+++ rcon/map_recorder.py
@@ -34,6 +34,8 @@ class MapsRecorder:
 
     def _restore_state(self):
         current_map = self.rcon.get_map()
+        self.prev_map = current_map
+
         try:
             last = self.maps_history[0]
         except IndexError:
@@ -43,9 +45,8 @@ class MapsRecorder:
         started_time = datetime.fromtimestamp(last.get("start"))
         elapsed_time = datetime.now() - started_time
 
-        if last.get("name") == current_map and elapsed_time < timedelta(hours=2):
+        if last.get("name") == current_map.replace('_RESTART', '') and elapsed_time < timedelta(minutes=90):
             logging.info("State recovered successfully")
-            self.prev_map = current_map
         else:
             logging.warning("The map recorder was offline for too long, the maps history will have gaps")
 
@@ -56,22 +57,23 @@ class MapsRecorder:
         except Exception:
             logger.info("Faied to get current map. Skipping")
             return 
+        
         logger.debug("Checking for map change current: %s prev: %s", current_map, self.prev_map)
-        if self.prev_map and current_map and self.prev_map != current_map:
-            if self.prev_map.replace('_RESTART', '') in ALL_MAPS:
+        if self.prev_map != current_map:
+            if self.prev_map and self.prev_map.replace('_RESTART', '') in ALL_MAPS:
                 self.maps_history.save_map_end(self.prev_map)
-            if current_map.replace('_RESTART', '') in ALL_MAPS:
+            if current_map and current_map.replace('_RESTART', '') in ALL_MAPS:
                 self.maps_history.save_new_map(current_map)
 
-            logger.info(
-                "Map change detected updating state. Prev map %s New Map %s",
-                self.prev_map,
-                current_map,
-            )
-            if not os.getenv('SILENT_MAP_RECORDER', None):
-                send_to_discord_audit(f"map change detected {dict_to_discord(dict(previous=self.prev_map, new=current_map))}", by="MAP_RECORDER", silent=False)
-            self.prev_map = current_map
-            self.last_map_change_time = datetime.now()
+                logger.info(
+                    "Map change detected updating state. Prev map %s New Map %s",
+                    self.prev_map,
+                    current_map,
+                )
+                if not os.getenv('SILENT_MAP_RECORDER', None):
+                    send_to_discord_audit(f"map change detected {dict_to_discord(dict(previous=self.prev_map, new=current_map))}", by="MAP_RECORDER", silent=False)
+                self.prev_map = current_map
+          
             return True
 
         return False
