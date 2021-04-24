d90518b8ae6f8b654694f7cd317f4dee2ab09f36
charles.loiseau <charles.loiseau@soprasteria.com>
2021-04-24 10:53:50 +0200

    Going full Mui

--- rcongui/src/components/PlayerInfo/PlayerInfo.js
+++ rcongui/src/components/PlayerInfo/PlayerInfo.js
@@ -1,7 +1,6 @@
 import {get, handle_http_errors, showResponse} from "../../utils/fetchUtils";
 import React from "react";
-import "./Player.css"
-import {Button, Popover} from "@material-ui/core";
+import {Avatar, Button, Grid, Popover} from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import {ExpandMore} from "@material-ui/icons";
 import moment from "moment";
@@ -24,9 +23,9 @@ const NamePopOver = ({names}) => {
     const id = open ? 'name-popover' : undefined;
 
     return (
-        <div>
+        <Grid item>
             <Button endIcon={<ExpandMore/>} onClick={handleClick}>
-                <div className="name-title">{names[0].name}</div>
+                <Typography variant={"h5"}>{names[0].name}</Typography>
             </Button>
             <Popover
                 id={id}
@@ -43,10 +42,10 @@ const NamePopOver = ({names}) => {
                 }}
             >
                 {names.map((name) => {
-                    return <Typography className="name">{name.name}</Typography>;
+                    return <Typography>{name.name}</Typography>;
                 })}
             </Popover>
-        </div>
+        </Grid>
     );
 }
 
@@ -133,7 +132,7 @@ class PlayerInfo extends React.Component {
      * fetch bans that currently affect the steamId64
      * @param steamId64
      */
-    #fetchPlayerBan = (steamId64) => {
+    fetchPlayerBan(steamId64) {
         get(`get_ban?steam_id_64=${steamId64}`)
             .then((data) => {
                 const temp = data.result.find((ban, index) => {
@@ -158,7 +157,7 @@ class PlayerInfo extends React.Component {
      * fetch Player data
      * @param steamId64
      */
-    #fetchPlayer = (steamId64) => {
+    fetchPlayer(steamId64) {
         get(`player?steam_id_64=${steamId64}`)
             .then((response) => showResponse(response, "get_user", false))
             .then((data) => {
@@ -182,84 +181,86 @@ class PlayerInfo extends React.Component {
             .catch(handle_http_errors);
     }
 
-    componentWillReceiveProps(nextProps){
-        const { steamId64 } = nextProps.match.params
-        if(steamId64 !== undefined){
-            this.#fetchPlayer(steamId64)
-            this.#fetchPlayerBan(steamId64)
-        }
-    }
-
     componentDidMount() {
-        const { steamId64 } = this.props.match.params
-        if(steamId64 !== undefined){
-            this.#fetchPlayer(steamId64)
-            this.#fetchPlayerBan(steamId64)
+        const {steamId64} = this.props.match.params
+        if (steamId64 !== undefined) {
+            this.fetchPlayer(steamId64)
+            this.fetchPlayerBan(steamId64)
         }
     }
 
     render() {
+        const {classes} = this.props
         return (
-            <div className="player-info">
-                <div>
-                    {(this.state.loaded) ?
-                        <div className="player">
-                            <div className="player-col-1">
-                                <div className="avatar">
-                                    {
-                                        (this.state.steaminfo?.profile?.avatarfull !== undefined) ?
-                                            <img src={this.state.steaminfo?.profile?.avatarfull} alt="player avatar"/>
-                                            :
-                                            <span className="letter">{this.state.names[0]?.name[0].toUpperCase()}</span>
-                                    }
+            <Grid container className={classes.root}>
 
-                                </div>
-                                <div>
-                                    <h3>Last connection</h3>
-                                    <div>{moment(this.state.sessions[0]?.end).format("ddd Do MMM HH:mm:ss")}</div>
-                                </div>
-                                <div>
-                                    <h3>Total play time</h3>
-                                    <div>{moment.duration(this.state.total_playtime_seconds, "seconds").humanize()}</div>
-                                </div>
-                                <div>
-                                    <h3>Player penalties</h3>
-                                    <div>Perma ban: {this.state.penalty_count.PERMABAN}</div>
-                                    <div>Temp ban: {this.state.penalty_count.TEMPBAN}</div>
-                                    <div>Kick: {this.state.penalty_count.KICK}</div>
-                                    <div>Punish: {this.state.penalty_count.PUNISH}</div>
-                                </div>
-                            </div>
+                {(this.state.loaded) ?
+                    <Grid item sm={12} className={classes.marginTop}>
+                        <Grid container>
+                            <Grid item sm={2}>
+                                <Grid container justify={"center"} alignContent={"center"} wrap={"wrap"}
+                                      direction={"column"} spacing={3}>
+                                    <Grid item>
+                                        <Avatar style={{height: '150px', width: '150px', fontSize: '5rem'}}
+                                                variant={"square"} className={classes.square}
+                                                src={this.state.steaminfo?.profile?.avatarfull}>
+                                            {this.state.names[0]?.name[0].toUpperCase()}
+                                        </Avatar>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant={"h6"}>Last connection</Typography>
+                                        <Typography>{moment(this.state.sessions[0]?.end).format("ddd Do MMM HH:mm:ss")}</Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant={"h6"}>Total play time</Typography>
+                                        <Typography>{moment.duration(this.state.total_playtime_seconds, "seconds").humanize()}</Typography>
+                                    </Grid>
+                                    <Grid item>
+                                        <Typography variant={"h6"}>Player penalties</Typography>
+                                        <Typography>Perma ban: {this.state.penalty_count.PERMABAN}</Typography>
+                                        <Typography>Temp ban: {this.state.penalty_count.TEMPBAN}</Typography>
+                                        <Typography>Kick: {this.state.penalty_count.KICK}</Typography>
+                                        <Typography>Punish: {this.state.penalty_count.PUNISH}</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
 
-                            <div className="player-col-2">
-                                <div className="names-list">
-                                    <NamePopOver names={this.state.names}/>
-                                </div>
-                                <div className="player-status">
-                                    <span className={`vip ${this.state.vip ? "active" : ""}`}>VIP</span>
-                                    <span className={`permaban ${this.state.perma ? "active" : ""}`}>PERMABAN</span>
-                                    <span className={`tempban ${this.state.temp ? "active" : ""}`}>TEMPBAN</span>
-                                    <span
-                                        className={`blacklisted ${this.state.blacklist?.is_blacklisted ? "active" : ""}`}>BLACKLISTED</span>
-                                </div>
-                                <div className="punishments">
-                                    <Punishment punishments={this.state.received_actions}/>
-                                </div>
+                            <Grid item sm={9}>
+                                <Grid container spacing={3} justify={"flex-start"} alignItems={"flex-start"}
+                                      alignContent={"flex-start"}>
+                                    <Grid item sm={12}>
+                                        <Grid container justify={"flex-start"} alignItems={"flex-start"}
+                                              alignContent={"flex-start"}>
+                                            <NamePopOver names={this.state.names}/>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item sm={4}>
+                                        <Grid container sm={12} justify={"space-between"}>
+                                            <Grid item><Typography color={this.state.vip ? "primary" : ""}
+                                                                   variant={"button"}>VIP</Typography></Grid>
+                                            <Grid item><Typography color={this.state.perma ? "error" : ""}
+                                                                   variant={"button"}>PERMABAN</Typography></Grid>
+                                            <Grid item><Typography color={this.state.temp ? "error" : ""}
+                                                                   variant={"button"}>TEMPBAN</Typography></Grid>
+                                            <Grid item><Typography
+                                                color={this.state.blacklist?.is_blacklisted ? "error" : ""}
+                                                variant={"button"}>BLACKLISTED</Typography></Grid>
+                                        </Grid>
+                                    </Grid>
+                                    <Grid item sm={12}>
+                                        <Punishment punishments={this.state.received_actions}/>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
 
-                            </div>
+                    :
+                    <div>pending</div>
+                }
 
 
-                        </div>
-
-                        :
-                        <div>pending</div>
-                    }
-
-                </div>
-                <div className="chat">
-                    <div>under construction</div>
-                </div>
-            </div>
+            </Grid>
         )
     }
 }
