9f83dd36e47de8dd4ae67cb67b69dab0b641bddc
timraay <traaymakers2005@gmail.com>
2021-09-11 18:16:18 +0200

    Update auto settings upgrade script

--- upgrade_auto_settings.py
+++ upgrade_auto_settings.py
@@ -1,60 +1,85 @@
+#!/usr/bin/env python
+
 import os, json, sys
+from rcon.auto_settings import CONFIG_DIR
+from rcon.user_config import AutoSettingsConfig
 
 # Setup logger
 import logging
 logging.basicConfig(format='[%(levelname)s] %(message)s', level=logging.INFO)
 logging.info('Upgrading auto settings...')
 
-CONFIG_DIR = sys.argv[1] if len(sys.argv) >= 2 else 'config/'
-logging.info('Using "%s" as config dir', CONFIG_DIR)
-if not os.path.exists(CONFIG_DIR):
-    logging.fatal('Invalid config dir, aborting...')
-    exit(1)
+"""
 
+To upgrade, run the below command for each server instance.
+Replace the container number with your server number.
+
+docker-compose exec backend_1 /code/upgrade_auto_settings.py
 
 """
 
-To upgrade, run the below command:
+def get_config(filename):
+    try:
+        with open("%s%s" % (CONFIG_DIR, filename)) as f:
+            return json.load(f)
+    except FileNotFoundError:
+        logging.info("Couldn't find config `config/%s`", filename)
+        return None
+    except json.JSONDecodeError:
+        logging.info("Invlid JSON in config `config/%s`", filename)
+        return None
+def find_config(server_number):
+    config = get_config(f"auto_settings_{server_number}.json")
+    if not config:
+        logging.info("No config for server number, falling back to common one")
+        config = get_config("auto_settings.json")
+    if not config:
+        logging.warning("Config 'auto_settings.json' not found. Falling back to default")
+        config = get_config("auto_settings.default.json")
+    if not config:
+        logging.error("Couldn't use default config 'auto_settings.default.json'")
+    return config
 
-python3 upgrade_auto_settings.py [config/]
 
-"""
+SERVER_NUMBER = os.getenv('SERVER_NUMBER', 0)
+if not SERVER_NUMBER:
+    logging.fatal("Couldn't find Server Number in environment")
+    sys.exit()
 
 
-for filename in os.listdir(CONFIG_DIR):
-    if filename.startswith('auto_settings'):
-        try:
-            with open(CONFIG_DIR+filename, 'r') as f:
-                config = json.load(f)
+config = find_config(SERVER_NUMBER)
+if not config:
+    logging.fatal("No auto settings config file associated with this server")
+    sys.exit()
 
-            new = dict(
-                always_apply_defaults=False,
-                defaults={command: params for (command, params) in config['player_count']['defaults']},
-                rules=[]
-            )
 
-            for i, (condition, params, commands) in enumerate(config['player_count']['rules']):
-                new_rule = dict(
-                    conditions={},
-                    commands={command: params for (command, params) in config['player_count']['defaults']}
-                )
-                
-                if condition == "between":
-                    min, max = params
-                    new_rule['conditions']['player_count'] = dict(min=min, max=max)
-                elif condition == "equals":
-                    val = params[0]
-                    new_rule['conditions']['player_count'] = dict(min=val, max=val)
-                else:
-                    logging.warning('Invalid condition for rules[%s], skipping condition...', i)
+try:
+    new = dict(
+        always_apply_defaults=False,
+        defaults={command: params for (command, params) in config['player_count']['defaults']},
+        rules=[]
+    )
 
-                new['rules'].append(new_rule)
-
-            with open(CONFIG_DIR+filename, 'w') as f:
-                json.dump(new, f, indent=2)
-
-        except Exception as e:
-            logging.error('Failed to upgrade %s: %s: %s', filename, e.__class__.__name__, e)
+    for i, (condition, params, commands) in enumerate(config['player_count']['rules']):
+        new_rule = dict(
+            conditions={},
+            commands={command: params for (command, params) in config['player_count']['defaults']}
+        )
         
+        if condition == "between":
+            min, max = params
+            new_rule['conditions']['player_count'] = dict(min=min, max=max)
+        elif condition == "equals":
+            val = params[0]
+            new_rule['conditions']['player_count'] = dict(min=val, max=val)
         else:
-            logging.info('Successfully upgraded %s', filename)
+            logging.warning('Invalid condition for rules[%s], skipping condition...', i)
+
+        new['rules'].append(new_rule)
+
+    AutoSettingsConfig().set_settings(new)        
+
+except Exception as e:
+    logging.error('Failed to upgrade auto settings: %s: %s', e.__class__.__name__, e)
+else:
+    logging.info('Successfully upgraded auto settings')
