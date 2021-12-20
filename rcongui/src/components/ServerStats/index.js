d140b887bfeb345355d7e9bd08f5c171657c30ea
Julien marechal <julien.marechal@imc.com>
2021-12-20 10:28:48 +0100

    Multi dataset + events

--- rcongui/src/components/ServerStats/index.js
+++ rcongui/src/components/ServerStats/index.js
@@ -9,11 +9,14 @@ import {
   Title,
   Tooltip,
   Legend,
+  registerables,
 } from "chart.js";
+import palette from "google-palette";
 import "chartjs-adapter-moment";
 import { Bar } from "react-chartjs-2";
 import { get, handle_http_errors, showResponse } from "../../utils/fetchUtils";
 import { fromJS } from "immutable";
+import { SportsRugbySharp } from "@material-ui/icons";
 
 ChartJS.register(
   CategoryScale,
@@ -26,44 +29,88 @@ ChartJS.register(
   Legend
 );
 
+function hexToRgb(hex) {
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result
+    ? "rgb(" +
+        parseInt(result[1], 16) +
+        "," +
+        parseInt(result[2], 16) +
+        "," +
+        parseInt(result[3], 16) +
+        ")"
+    : null;
+}
+
 const ServerStatsPage = ({ classes }) => {
-  const [stats, setStats] = React.useState([]);
+  const [stats, setStats] = React.useState({});
+  const colors = React.useMemo(
+    () =>
+      palette("tol-rainbow", stats ? Object.keys(stats).length : 10).map(
+        function (hex) {
+          const s = hexToRgb("#" + hex);
+          console.log(s);
+          return s;
+        }
+      ),
+    [stats]
+  );
+  
+  const datasets = React.useMemo(() => Object.keys(stats).map((mapName, i) => ({
+    grouped: true,
+    label: mapName,
+    data: stats[mapName],
+    backgroundColor: colors[i],
+    borderColor: colors[i],
+    fill: true,
+  })), [stats, colors])
 
   React.useEffect(() => {
     get(`get_server_stats`)
-      .then((res) => showResponse(res, "get_server_stats", false))
+      .then((res) => showResponse(res, "get_server_stats?by_map=true", false))
       .then((data) => (data.result && data.result ? setStats(data.result) : ""))
       .catch(handle_http_errors);
   }, []);
 
   console.log("Stats: ", stats);
-  return stats.length > 0 ? (
+  return stats ? (
     <Bar
       options={{
+        onClick: (e, el) => {
+          if (el.length > 0) {
+            console.log(el)
+            const clickedEl = el[0]
+            const dataPoint = datasets[clickedEl.datasetIndex].data[clickedEl.index]
+            console.log(dataPoint)
+          }
+         
+        },
         plugins: {
           tooltip: {
             callbacks: {
+              
               afterTitle: function (context) {
                 if (context && context[0]?.raw?.map) {
-                    return context[0]?.raw?.map
+                  return context[0]?.raw?.map;
                 }
-                return "Can't get map info"
+                return "Can't get map info";
               },
               footer: function (context) {
                 if (context && context[0]?.raw?.players?.length > 0) {
-                  console.log("here")
-                  return context[0]?.raw.players.slice(0, 10)?.join('\n') + '\n...\nClick for full list'
-                
+                  return (
+                    context[0]?.raw.players.slice(0, 10)?.join("\n") +
+                    "\n...\nClick for full list"
+                  );
                 }
-                
+
                 return "Can't get player list";
               },
             },
           },
         },
         scales: {
-          x: { type: "time" },
-          y: { min: 0, max: 100 },
+          x: { type: "time", stacked: true },
+          y: { min: 0, max: 100, stacked: true },
         },
         parsing: {
           xAxisKey: "minute",
@@ -71,14 +118,7 @@ const ServerStatsPage = ({ classes }) => {
         },
       }}
       data={{
-        datasets: [
-          {
-            label: "PlayerCount",
-            data: stats,
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-          },
-        ],
+        datasets: datasets,
       }}
     />
   ) : (
--- rconweb/api/server_stats.py
+++ rconweb/api/server_stats.py
@@ -1,3 +1,4 @@
+from os import stat
 from rcon.player_history import get_players_by_time
 from rcon.models import Maps, enter_session, PlayerSession, PlayerName, PlayerSteamID
 import datetime
@@ -10,11 +11,14 @@ from rcon.settings import SERVER_INFO
 from django.views.decorators.csrf import csrf_exempt
 from .auth import api_response, login_required
 from rcon.cache_utils import ttl_cache
+from .views import ctl, _get_data
 
 @csrf_exempt
 def get_server_stats(request):
+    #data = _get_data(request)
+
     return api_response(
-        result=_get_server_stats(), error=None, failed=False, command="get_server_stats"
+        result=_get_server_stats(by_map=True), error=None, failed=False, command="get_server_stats"
     )
 
 
@@ -23,6 +27,9 @@ def trunc_datetime_to_hour(dt):
 
 
 def index_map_per_hour(maps):
+    """
+    To quickly lookup which map was running at a certain time we index them by hours
+    """
     indexed_map_by_hours = {}
 
     for map_ in maps:
@@ -41,12 +48,13 @@ def index_map_per_hour(maps):
 
     return indexed_map_by_hours
 
-@ttl_cache(60 * 10)
-def _get_server_stats(start=None, end=None):
+
+@ttl_cache(60)
+def _get_server_stats(start=None, end=None, by_map=False):
     if start is None:
-        start = datetime.datetime.now() - datetime.timedelta(hours=24)
+        start = datetime.datetime.now() - datetime.timedelta(hours=60)
     if end is None:
-        end = datetime.datetime.now()
+        end = datetime.datetime.now() - datetime.timedelta(hours=0)
 
     if start > end:
         raise ValueError("Start time can't be after end time")
@@ -55,6 +63,8 @@ def _get_server_stats(start=None, end=None):
     end.replace(second=0, microsecond=0)
 
     with enter_session() as sess:
+        # Crete a list of minutes for the given time window
+        # Bear in mind that a huge window will impact perf a lot
         series = pd.date_range(start=start, end=end, freq="T")
         series = series.to_list()
 
@@ -65,6 +75,7 @@ def _get_server_stats(start=None, end=None):
         )
         indexed_map_hours = index_map_per_hour(maps)
 
+        # Get all players withing the time window
         q = (
             sess.query(PlayerSession)
             .filter(
@@ -77,8 +88,13 @@ def _get_server_stats(start=None, end=None):
         )
         sessions = q.all()
 
-        stat = []
+        stats = []
+        if by_map:
+            stats = {}
         for minute in series:
+            # For every minute of the time window we add:
+            # The map, and all the players that had a session at that minute
+            # This algo is quite crappy but it works decently enough
             present_players = []
             map_ = None
             # find the map that was running at that minute
@@ -94,12 +110,17 @@ def _get_server_stats(start=None, end=None):
                 session_end = player_session.end or end
                 if player_session.start <= minute <= session_end:
                     present_players.append(player_session.steamid.names[0].name)
-            stat.append(
-                {
-                    "minute": minute,
-                    "map": map_.map_name if map_ else None,
-                    "count": len(present_players),
-                    "players": present_players,
-                }
-            )
-    return stat
+
+            map_name = map_.map_name if map_ else None
+            item = {
+                "minute": minute,
+                "map": map_name,
+                "count": len(present_players),
+                "players": present_players,
+            }
+            if by_map:
+                stats.setdefault(map_name, []).append(item)
+            else:
+                stats.append(item)
+    print(stats)
+    return stats
