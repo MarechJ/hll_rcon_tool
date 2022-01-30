b34d3ce3ebda48674b836dca8d2a343ee354c257
Julien Marechal <julien.marechal35@gmail.com>
2022-01-30 13:48:02 +0100

    Add command to re-process game stats #minor

--- rcon/workers.py
+++ rcon/workers.py
@@ -97,58 +97,62 @@ def record_stats(map_info):
     except Exception:
         logger.exception("Unexpected error while recording stats for %s", map_info)
 
-def _record_stats(map_info):
-    stats = TimeWindowStats()
-
+def _record_stats(map_info=None, map=None):
     start = datetime.datetime.utcfromtimestamp(map_info.get('start'))
     end = datetime.datetime.utcfromtimestamp(map_info.get('end'))
-
+    
     if not start or not end:
         logger.error("Can't record stats, no time info for %s", map_info)
         return
 
     with enter_session() as sess:
-        map = get_or_create_map(sess=sess, start=start, end=end, server_number=os.getenv("SERVER_NUMBER"), map_name=map_info["name"])
-        player_stats = stats.get_players_stats_at_time(from_=start, until=end)
-        for player, stats in player_stats.items():
-            if steam_id_64 := stats.get("steam_id_64"):
-                player_record = get_player(sess, steam_id_64=steam_id_64)
-                if not player_record:
-                    logger.error("Can't find DB record for %s", steam_id_64)
-                    continue
-                
-                player_stats = dict(
-                    playersteamid_id=player_record.id,
-                    map_id=map.id,
-                    name=stats.get("player"),
-                    kills=stats.get("kills"),
-                    kills_streak=stats.get("kills_streak"),
-                    deaths=stats.get("deaths"),
-                    deaths_without_kill_streak=stats.get("deaths_without_kill_streak"),
-                    teamkills=stats.get("teamkills"),
-                    teamkills_streak=stats.get("teamkills_streak"),
-                    deaths_by_tk=stats.get("deaths_by_tk"),
-                    deaths_by_tk_streak=stats.get("deaths_by_tk_streak"),
-                    nb_vote_started=stats.get("nb_vote_started"),
-                    nb_voted_yes=stats.get("nb_voted_yes"),
-                    nb_voted_no=stats.get("nb_voted_no"),
-                    time_seconds=stats.get("time_seconds"),
-                    kills_per_minute=stats.get("kills_per_minute"),
-                    deaths_per_minute=stats.get("deaths_per_minute"),
-                    kill_death_ratio=stats.get("kill_death_ratio"),
-                    longest_life_secs=stats.get("longest_life_secs"),
-                    shortest_life_secs=stats.get("shortest_life_secs"),
-                    weapons=stats.get("weapons"),
-                    most_killed=stats.get("most_killed"),
-                    death_by=stats.get("death_by")
-                )
-                logger.debug(f"Saving stats %s", player_stats)
-                player_stat_record = PlayerStats(
-                   **player_stats
-                )
-                sess.add(player_stat_record)
-            else:
-                logger.error("Stat object does not contain a steam id: %s", stats)
+        map_ = get_or_create_map(sess=sess, start=start, end=end, server_number=os.getenv("SERVER_NUMBER"), map_name=map_info["name"])
+        record_stats_from_map(sess, map_)
+        sess.commit()
+
+
+def record_stats_from_map(sess, map_):
+    stats = TimeWindowStats()
+    player_stats = stats.get_players_stats_at_time(from_=map_.start, until=map_.end, server_number=str(map_.server_number))
+    for player, stats in player_stats.items():
+        if steam_id_64 := stats.get("steam_id_64"):
+            player_record = get_player(sess, steam_id_64=steam_id_64)
+            if not player_record:
+                logger.error("Can't find DB record for %s", steam_id_64)
+                continue
+            
+            player_stats = dict(
+                playersteamid_id=player_record.id,
+                map_id=map_.id,
+                name=stats.get("player"),
+                kills=stats.get("kills"),
+                kills_streak=stats.get("kills_streak"),
+                deaths=stats.get("deaths"),
+                deaths_without_kill_streak=stats.get("deaths_without_kill_streak"),
+                teamkills=stats.get("teamkills"),
+                teamkills_streak=stats.get("teamkills_streak"),
+                deaths_by_tk=stats.get("deaths_by_tk"),
+                deaths_by_tk_streak=stats.get("deaths_by_tk_streak"),
+                nb_vote_started=stats.get("nb_vote_started"),
+                nb_voted_yes=stats.get("nb_voted_yes"),
+                nb_voted_no=stats.get("nb_voted_no"),
+                time_seconds=stats.get("time_seconds"),
+                kills_per_minute=stats.get("kills_per_minute"),
+                deaths_per_minute=stats.get("deaths_per_minute"),
+                kill_death_ratio=stats.get("kill_death_ratio"),
+                longest_life_secs=stats.get("longest_life_secs"),
+                shortest_life_secs=stats.get("shortest_life_secs"),
+                weapons=stats.get("weapons"),
+                most_killed=stats.get("most_killed"),
+                death_by=stats.get("death_by")
+            )
+            logger.debug(f"Saving stats %s", player_stats)
+            player_stat_record = PlayerStats(
+                **player_stats
+            )
+            sess.add(player_stat_record)
+        else:
+            logger.error("Stat object does not contain a steam id: %s", stats)
 
 
 def get_job_results(job_key):
