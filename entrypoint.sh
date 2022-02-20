a49fa0c3ab67ca8f22615943d6857256ece038ed
Julien Marechal <julien.marechal@blah.com>
2022-02-20 01:19:45 +0100

    auto process history server stats

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,8 @@ then
     exit 0
 fi
   sleep 10
-  python -m rcon.cli record_server_stats
+  python -m rcon.cli record_server_stats_inception &
+  python -m rcon.cli record_server_stats &
   env >> /etc/environment
   supervisord -c /config/supervisord_$SERVER_NUMBER.conf || supervisord -c /config/supervisord.conf
 fi
