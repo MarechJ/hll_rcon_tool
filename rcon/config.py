8da9fdefe3d3d7cdc7620967b3fc48eeddb6050c
Julien Marechal <julien.marechal35@gmail.com>
2022-06-10 17:48:21 +0200

    Ignore None squad and support server specific config

--- rcon/config.py
+++ rcon/config.py
@@ -12,7 +12,11 @@ logger = logging.getLogger(__name__)
 def get_config():
     root = os.getenv('CONFIG_DIR', 'config/')
     default_config_path = Path(root) / Path('default_config.yml')
-    user_config_path = Path(root) / Path('config.yml')
+    server_config_path = Path(root) / Path(f'config_{os.getenv("SERVER_NUMBER", 0)}.yml')
+    if server_config_path.exists():
+        user_config_path = server_config_path
+    else:
+        user_config_path = Path(root) / Path('config.yml')
     user_config = {}
     try:
         with default_config_path.open() as f:
@@ -25,9 +29,11 @@ def get_config():
         raise
     try:
         with user_config_path.open() as f:
+            logger.info("Loading config: %s", user_config_path)
             user_config = yaml.safe_load(stream=f)
     except FileNotFoundError:
         logger.warning("No user config found, defaults only are loaded")
+    except yaml.YAMLError:
         logger.error("User config at '%s' is invalid YAML", str(user_config_path))
 
     config.update(user_config)
