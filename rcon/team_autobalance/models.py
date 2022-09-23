bf4b54a3369b0cd0f9dbb0a8c9a74868bcdedda8
Christopher Mathey <emathey@protonmail.com>
2022-09-23 05:18:52 -0600

    Move types from autobalance.py

--- rcon/team_autobalance/models.py
+++ rcon/team_autobalance/models.py
@@ -1,5 +1,6 @@
-from dataclasses import dataclass, field  # TODO: replace with pydantic data classes
-from typing import List, Callable
+# TODO: replace with pydantic data classes
+from dataclasses import dataclass, field
+from typing import Any, Callable, Dict, List, NewType
 
 VALID_AUTOBALANCE_METHODS = ("random", "arrival_most_recent", "arrival_least_recent")
 
@@ -20,3 +21,20 @@ class AutoBalanceConfig:
     immuned_roles: Callable[[], List[str]] = field(
         default_factory=tuple
     )  # TODO: fix type annotation
+
+
+DetailedPlayerInfo = NewType("DetailedPlayerInfo", Dict[str, Any])
+"""
+        rcon.extended_commands.get_detailed_player_info()
+        
+        Name: T17 Scott
+        steamID64: 01234567890123456
+        Team: Allies            # "None" when not in team
+        Role: Officer           
+        Unit: 0 - Able          # Absent when not in unit
+        Loadout: NCO            # Absent when not in team
+        Kills: 0 - Deaths: 0
+        Score: C 50, O 0, D 40, S 10
+        Level: 34
+
+        """
