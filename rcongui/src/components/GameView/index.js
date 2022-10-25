b3524520cea8d63352754f1d0ad2dd46f78f2876
Christopher Mathey <emathey@protonmail.com>
2022-10-25 11:47:59 +0400

    Add missing imports/closing tags from committing chunks

--- rcongui/src/components/GameView/index.js
+++ rcongui/src/components/GameView/index.js
@@ -28,29 +28,34 @@ import {
   TextField,
   InputAdornment,
 } from "@material-ui/core";
-import WarningIcon from '@material-ui/icons/Warning';
+import WarningIcon from "@material-ui/icons/Warning";
 import { fromJS, Map, List as IList } from "immutable";
-import { makeStyles } from '@material-ui/core/styles';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import DraftsIcon from '@material-ui/icons/Drafts';
-import SendIcon from '@material-ui/icons/Send';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import StarBorder from '@material-ui/icons/StarBorder';
-import { PlayerItem, KDChips, ScoreChips } from '../PlayerView/playerList'
+import { makeStyles } from "@material-ui/core/styles";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import DraftsIcon from "@material-ui/icons/Drafts";
+import SendIcon from "@material-ui/icons/Send";
+import ExpandLess from "@material-ui/icons/ExpandLess";
+import ExpandMore from "@material-ui/icons/ExpandMore";
+import StarBorder from "@material-ui/icons/StarBorder";
+import { PlayerItem, KDChips, ScoreChips } from "../PlayerView/playerList";
 import reactRouterDom from "react-router-dom";
-import { AlertDialog, InputSlider, Padlock } from "../commonComponent";
+import { Padlock } from "../commonComponent";
 import FormControl from "@material-ui/core/FormControl";
+import {
+  get,
+  handle_http_errors,
+  postData,
+  showResponse,
+} from "../../utils/fetchUtils";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-
     backgroundColor: theme.palette.background.paper,
   },
   nested: {
@@ -65,10 +70,9 @@ const useStyles = makeStyles((theme) => ({
     height: theme.spacing(7),
   },
   primaryBackground: {
-    backgroundColor: theme.palette.primary.dark
+    backgroundColor: theme.palette.primary.dark,
   },
 }));
-
 
 const Squad = ({ classes: globalClasses, squadName, squadData, doOpen }) => {
   const classes = useStyles();
@@ -79,81 +83,129 @@ const Squad = ({ classes: globalClasses, squadName, squadData, doOpen }) => {
   const sizes = {
     armor: 3,
     infantry: 6,
-    recon: 2
-  }
+    recon: 2,
+  };
 
   if (squadName === "commander") return "";
 
-  return squadData ? <Fragment>
-    <ListItem button onClick={handleClick}      >
-      <ListItemIcon>
-        <Avatar variant="rounded" className={classes.primaryBackground} alt={squadData.get("type", "na")} src={`icons/roles/${squadData.get("type")}.png`}>
-          {squadName[0].toUpperCase()}
-        </Avatar>
-      </ListItemIcon>
-      <ListItemText primary={
+  return squadData ? (
+    <Fragment>
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon>
+          <Avatar
+            variant="rounded"
+            className={classes.primaryBackground}
+            alt={squadData.get("type", "na")}
+            src={`icons/roles/${squadData.get("type")}.png`}
+          >
+            {squadName[0].toUpperCase()}
+          </Avatar>
+        </ListItemIcon>
+        <ListItemText
+          primary={
+            <Typography variant="h6">
+              {`${squadName.toUpperCase()} - ${
+                squadData.get("players", new IList()).size
+              }/${sizes[squadData.get("type", "infantry")]}`}{" "}
+              {squadData.get("has_leader", false) ? (
+                ""
+              ) : (
+                <WarningIcon
+                  style={{ verticalAlign: "middle" }}
+                  fontSize="small"
+                  color="error"
+                />
+              )}
+            </Typography>
+          }
+          secondary={
+            <Grid container spacing={1}>
+              <ScoreChips
+                backgroundClass={classes.primaryBackground}
+                player={squadData}
+              />
+              <KDChips classes={classes} player={squadData} />
+            </Grid>
+          }
+        />
 
-        <Typography variant="h6">
-          {`${squadName.toUpperCase()} - ${squadData.get("players", new IList()).size}/${sizes[squadData.get("type", "infantry")]}`} {squadData.get("has_leader", false) ? "" : <WarningIcon style={{ verticalAlign: "middle" }} fontSize="small" color="error" />}
-        </Typography>
-
-      }
-        secondary={<Grid container spacing={1}><ScoreChips backgroundClass={classes.primaryBackground} player={squadData} /><KDChips classes={classes} player={squadData} /></Grid>} />
-
-      {/*<ListItemSecondaryAction>
+        {/*<ListItemSecondaryAction>
         <Checkbox
           edge="end"
         />
       </ListItemSecondaryAction> */}
-
-    </ListItem>
-    <Collapse in={open || doOpen} timeout="auto" unmountOnExit>
-      <List component="div" disablePadding className={classes.nested}>
-        {squadData.get("players", new IList()).map(
-          (player) => (
+      </ListItem>
+      <Collapse in={open || doOpen} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding className={classes.nested}>
+          {squadData.get("players", new IList()).map((player) => (
             <PlayerItem
               key={player.get("name")}
               classes={globalClasses}
               player={player}
               playerHasExtraInfo={true}
-              onDeleteFlag={() => (null)}
+              onDeleteFlag={() => null}
             />
-          )
-        )}
-      </List>
-    </Collapse>
-  </Fragment> : ""
-}
+          ))}
+        </List>
+      </Collapse>
+    </Fragment>
+  ) : (
+    ""
+  );
+};
 
 const Team = ({ classes: globalClasses, teamName, teamData }) => {
   const classes = useStyles();
-  const [openAll, setOpenAll] = React.useState(false)
-  const onOpenAll = () => openAll ? setOpenAll(false) : setOpenAll(true)
+  const [openAll, setOpenAll] = React.useState(false);
+  const onOpenAll = () => (openAll ? setOpenAll(false) : setOpenAll(true));
 
-  return teamData ? <List
-    dense
-    component="nav"
-    subheader={
-      <ListSubheader component="div" id="nested-list-subheader">
-        <Typography variant="h4">{teamName} {teamData.get("count", 0)}/50 <Link onClick={onOpenAll} component="button">{openAll ? "Collapse" : "Expand"} all</Link></Typography>
+  return teamData ? (
+    <List
+      dense
+      component="nav"
+      subheader={
+        <ListSubheader component="div" id="nested-list-subheader">
+          <Typography variant="h4">
+            {teamName} {teamData.get("count", 0)}/50{" "}
+            <Link onClick={onOpenAll} component="button">
+              {openAll ? "Collapse" : "Expand"} all
+            </Link>
+          </Typography>
+        </ListSubheader>
+      }
+      className={classes.root}
+    >
+      {teamData.get("commander") ? (
+        <PlayerItem
+          classes={globalClasses}
+          player={teamData.get("commander")}
+          playerHasExtraInfo={true}
+          onDeleteFlag={() => null}
+          avatarBackround={classes.primaryBackground}
+        />
+      ) : (
+        ""
+      )}
+      {teamData
+        .get("squads", new Map())
+        .toOrderedMap()
+        .sortBy((v, k) => k)
+        .entrySeq()
+        .map(([key, value]) => (
+          <Squad
+            key={key}
+            squadName={key}
+            squadData={value}
+            classes={globalClasses}
+            doOpen={openAll}
+          />
+        ))}
+    </List>
+  ) : (
+    ""
+  );
+};
 
-      </ListSubheader>
-    }
-    className={classes.root}
-  >
-    {teamData.get("commander") ?
-      <PlayerItem
-        classes={globalClasses}
-        player={teamData.get("commander")}
-        playerHasExtraInfo={true}
-        onDeleteFlag={() => (null)}
-        avatarBackround={classes.primaryBackground}
-      /> : ""}
-    {teamData.get("squads", new Map()).toOrderedMap().sortBy((v, k) => k).entrySeq().map(
-      ([key, value]) => (
-        <Squad key={key} squadName={key} squadData={value} classes={globalClasses} doOpen={openAll} />
-      )
-    )}
 const ShuffleForm = () => {
   const [shuffleMethod, setShuffleMethod] = React.useState("");
 
@@ -321,7 +373,7 @@ const BalanceForm = () => {
               type="number"
               onChange={(event) =>
                 handleNumberTextFieldChange(event, setImmuneSeconds)
-}
+              }
             />
           </Grid>
         </Grid>
@@ -374,30 +426,34 @@ const BalanceForm = () => {
 
 const GameView = ({ classes: globalClasses }) => {
   const classes = useStyles();
-  const [isLoading, setIsLoading] = React.useState(false)
-  const [teamView, setTeamView] = React.useState(null)
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [teamView, setTeamView] = React.useState(null);
 
   const loadData = () => {
-    setIsLoading(true)
+    setIsLoading(true);
     get("get_team_view_fast")
-    .then((response) => showResponse(response, "get_team_view_fast"))
-    .then((data) => {setIsLoading(false); if (data.result) {setTeamView(fromJS(data.result))}})
-    .catch(handle_http_errors);
-  }
+      .then((response) => showResponse(response, "get_team_view_fast"))
+      .then((data) => {
+        setIsLoading(false);
+        if (data.result) {
+          setTeamView(fromJS(data.result));
+        }
+      })
+      .catch(handle_http_errors);
+  };
 
   React.useEffect(() => {
     loadData();
-    const handle = setInterval(loadData, 15000)
-    return () => clearInterval(handle)
-  }, [])
-
+    const handle = setInterval(loadData, 15000);
+    return () => clearInterval(handle);
+  }, []);
 
   const onConfirm = () => console.log("Confirmed");
 
   return (
     <Grid container spacing={2} className={globalClasses.padding}>
       {teamView ? (
-      <Fragment>
+        <Fragment>
           {isLoading ? (
             <Grid item xs={12} className={globalClasses.doublePadding}>
               <LinearProgress />
@@ -413,28 +469,28 @@ const GameView = ({ classes: globalClasses }) => {
               <ShuffleForm />
             </Grid>
           </Grid>
-        <Grid item xs={12} md={6}>
+          <Grid item xs={12} md={6}>
             <Team
               classes={globalClasses}
               teamName="Axis"
               teamData={teamView.get("axis")}
             />
-        </Grid>
-        <Grid item xs={12} md={6}>
+          </Grid>
+          <Grid item xs={12} md={6}>
             <Team
               classes={globalClasses}
               teamName="Allies"
               teamData={teamView.get("allies")}
             />
-        </Grid>
-        <Grid item xs={12} md={6}>
+          </Grid>
+          <Grid item xs={12} md={6}>
             <Team
               classes={globalClasses}
               teamName="Unassigned"
               teamData={teamView.get("none")}
             />
-        </Grid>
-      </Fragment>
+          </Grid>
+        </Fragment>
       ) : (
         <Grid item xs={12} className={globalClasses.doublePadding}>
           <LinearProgress />
