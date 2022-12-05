e8f52f35480010e99a0784069b0f9d147756f3c3
Florian <florian.schmidt.welzow@t-online.de>
2022-12-05 22:54:28 +0100

    merge branches in alembic as well

---
+++ alembic/versions/23fd01b846e8_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 23fd01b846e8
+Revises: 102580ac9fea, e87989f4783a
+Create Date: 2022-12-05 22:47:12.868963
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '23fd01b846e8'
+down_revision = ('102580ac9fea', 'e87989f4783a')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
