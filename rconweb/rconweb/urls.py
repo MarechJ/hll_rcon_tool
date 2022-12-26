e3708cd9d5b7b270d61bde94e3c1d9cd1b702e4c
Julien Marechal <julien.marechal@blah.com>
2022-12-26 19:34:23 +0100

    Update readme and change logs urls

--- rconweb/rconweb/urls.py
+++ rconweb/rconweb/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
-    path("logs/", include("directory.urls")),
+    path("api/logs/", include("directory.urls")),
 ]
