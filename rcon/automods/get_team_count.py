1a3ff7cc08f4f50bd601ff6308a7940e1c649092
Florian <florian.schmidt.welzow@t-online.de>
2022-12-29 17:51:20 +0100

    really fix counting teams side that is empty

--- rcon/automods/get_team_count.py
+++ rcon/automods/get_team_count.py
@@ -1,5 +1,5 @@
 def get_team_count(team_view, team):
-    if team_view[team] is None:
+    if team not in team_view:
         return 0
     return sum(
         len(s.get("players", [])) for s in team_view[team].get("squads", {}).values()
