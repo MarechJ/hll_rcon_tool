60e1b8a25649f388a94b4d0c21a4aaa10ac412e6
OlolBzH <31800740+OlolBzH@users.noreply.github.com>
2023-06-04 01:46:35 +0200

    Feature/level kicks (#3)
    Add auto kick by minimum level at player's connection feature

--- rcon/__init__.py
+++ rcon/__init__.py
@@ -1,5 +1,5 @@
 # Just make sure hooks are imported
 from rcon import hooks
-from rcon.auto_kick import auto_kick
+from rcon import auto_kick
 from rcon.discord_chat import handle_on_chat, handle_on_kill, handle_on_tk
 from rcon.watchlist import watchdog
