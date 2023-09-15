9da4dc8569937268bf7c5fd7933dbcd6e70b4085
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2023-09-15 12:46:17 +0200

    Update models.py

--- rcon/automods/models.py
+++ rcon/automods/models.py
@@ -372,6 +372,7 @@ class LevelThresholdsConfig:
 class NoSoloTankConfig:
     enabled: bool = False
     dry_run: bool = True
+    whitelist_flags: List = field(default_factory=lambda: ['🚨'])
     discord_webhook_url: str = ""
 
     number_of_notes: int = 2  # Let's give the mates some time to arrive
