871f0a3da0c832bf6786ab61f162c482c00daf95
C. Eric Mathey <48801688+cemathey@users.noreply.github.com>
2023-10-24 13:11:00 -0600

    Feat/api keys (#295)
    Adds the ability to authenticate with an API key and the HTTP authorization header instead of having to include a valid sessionid cookie with every request.

---
+++ rconweb/static/api/js/formset_handlers.js
@@ -0,0 +1,18 @@
+document.addEventListener('formset:added', (event) => {
+    const formsetName = event.detail.formsetName
+    if (formsetName === 'djangoapikey_set') {
+        const apiKey = document.getElementById(`id_${event.target.id}-api_key`)
+        apiKey.value = window.uuidv4()
+    }    
+});
+
+document.addEventListener('readystatechange', event => { 
+
+    if (event.target.readyState === "complete") {
+        const elementSearch = `id_api_key`
+        const apiKey = document.getElementById(elementSearch)
+        if (!apiKey.value) {
+            apiKey.value = window.uuidv4()
+        }
+    }
+});
