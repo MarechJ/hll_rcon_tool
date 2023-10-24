aef5c265fc2853ff4573302bcc266563b3e86e37
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2023-10-24 02:58:18 +0200

    Update level_thresholds.py

--- rcon/automods/level_thresholds.py
+++ rcon/automods/level_thresholds.py
@@ -216,7 +216,7 @@ class LevelThresholdsAutomod:
         self.logger.info("Squad %s %s", squad_name, squad)
         punitions_to_apply = PunitionsToApply()
         if not squad_name:
-            self.logger.info("Skipping None or empty squad %s %s", squad_name, squad)
+            self.logger.debug("Skipping None or empty squad %s %s", squad_name, squad)
             return punitions_to_apply
 
         server_player_count = get_team_count(team_view, "allies") + get_team_count(
