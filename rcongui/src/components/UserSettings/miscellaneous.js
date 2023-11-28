6b55a564a75bd27847ca59c04a270d00f5d8534d
C. Eric Mathey <48801688+cemathey@users.noreply.github.com>
2023-11-28 16:22:08 -0700

    Add documentation for the server name change config (#385)

--- rcongui/src/components/UserSettings/miscellaneous.js
+++ rcongui/src/components/UserSettings/miscellaneous.js
@@ -476,3 +476,42 @@ export const ExpiredVIP = ({
     />
   );
 };
+
+export const GTXNameChange = ({
+  description,
+  getEndpoint,
+  setEndpoint,
+  validateEndpoint,
+  describeEndpoint,
+}) => {
+  const notes = `
+    {
+        /*
+            This feature allows servers hosted by GTX to change their server name without restarting the
+            game server, or having to do it through their game server panel.
+
+            Your IP and SFTP port can be found in your game server panel.
+
+            The GSP username/password must be set in your .env
+        */
+
+        /* This is the IP address of your game server */
+        "ip": "127.0.0.1",
+
+        /* This is the SFTP port for your game server */
+        "port": 9933
+          
+    }
+    `;
+
+  return (
+    <UserSetting
+      description={description}
+      getEndpoint={getEndpoint}
+      setEndpoint={setEndpoint}
+      validateEndpoint={validateEndpoint}
+      describeEndpoint={describeEndpoint}
+      notes={notes}
+    />
+  );
+};
