07199dd6961864326e77c215f45be3816a2f66d8
C. Eric Mathey <emathey@protonmail.com>
2023-12-08 13:48:55 -0700

    Fix VIP uploads

--- rconweb/api/utils.py
+++ rconweb/api/utils.py
@@ -12,11 +12,15 @@ logger = getLogger(__name__)
 def _get_data(request: HttpRequest) -> QueryDict | dict[str, Any]:
     if request.method == "GET":
         return request.GET
-
-    # Don't silently swallow JSON parsing errors
-    # login_required decorator will return a reasonable API response on failure
-    # endpoints that don't require login should not be accepting POST requests
-    return json.loads(request.body)
+    # This is only used (currently) by the async_upload_vips endpoint which doesn't
+    # include a parseable JSON body, but form data and it pulls the data itself
+    elif request.method == "POST" and request.FILES:
+        return {}
+    else:
+        # Don't silently swallow JSON parsing errors
+        # login_required decorator will return a reasonable API response on failure
+        # endpoints that don't require login should not be accepting POST requests
+        return json.loads(request.body)
 
 
 def allow_csv(endpoint):
