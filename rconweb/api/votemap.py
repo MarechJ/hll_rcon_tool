b25c17e4ed140ab00f7fd8d9a278a928ebfdcb5e
C. Eric Mathey <emathey@protonmail.com>
2024-02-06 11:33:34 -0700

    Fix imports

--- rconweb/api/votemap.py
+++ rconweb/api/votemap.py
@@ -1,18 +1,11 @@
 from logging import getLogger
 
-from django.contrib.auth.decorators import permission_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
 from rcon.user_config.vote_map import VoteMapUserConfig
-from rcon.vote_map import VoteMap
 
-from .audit_log import record_audit
 from .auth import api_response, login_required
-from .decorators import require_content_type
-from .user_settings import _validate_user_config
-from .utils import _get_data
-from .views import audit
 
 logger = getLogger(__name__)
 
