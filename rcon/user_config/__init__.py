024c817a6411a516e4a462dc1cd7e24650d60180
C. Eric Mathey <48801688+cemathey@users.noreply.github.com>
2024-04-02 10:41:57 -0600

    Add a maintenance container to run database migrations only once regardless of number of backends (#512)
    Co-authored-by: ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>

--- rcon/user_config/__init__.py
+++ rcon/user_config/__init__.py
@@ -1,27 +1,31 @@
-from . import (
-    auto_broadcast,
-    auto_kick,
-    auto_mod_level,
-    auto_mod_no_leader,
-    auto_mod_seeding,
-    auto_mod_solo_tank,
-    ban_tk_on_connect,
-    camera_notification,
-    expired_vips,
-    gtx_server_name,
-    log_line_webhooks,
-    name_kicks,
-    rcon_connection_settings,
-    rcon_server_settings,
-    real_vip,
-    scorebot,
-    standard_messages,
-    steam,
-    vac_game_bans,
-    vote_map,
-    webhooks,
-)
+import os
 
-# Import all the sub modules that represent a user setting
-# so that .utils.all_subclasses() can properly report subclasses
-# for exporting settings through the CLI
+# Allows importing the models in the maintenance container to run database migrations
+if not os.getenv("HLL_MAINTENANCE_CONTAINER"):
+
+    # Import all the sub modules that represent a user setting
+    # so that .utils.all_subclasses() can properly report subclasses
+    # for exporting settings through the CLI
+    from . import (
+        auto_broadcast,
+        auto_kick,
+        auto_mod_level,
+        auto_mod_no_leader,
+        auto_mod_seeding,
+        auto_mod_solo_tank,
+        ban_tk_on_connect,
+        camera_notification,
+        expired_vips,
+        gtx_server_name,
+        log_line_webhooks,
+        name_kicks,
+        rcon_connection_settings,
+        rcon_server_settings,
+        real_vip,
+        scorebot,
+        standard_messages,
+        steam,
+        vac_game_bans,
+        vote_map,
+        webhooks,
+    )
