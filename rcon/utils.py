8037eb554e52acc81a13e90213875f11ae85ab43
C. Eric Mathey <emathey@protonmail.com>
2024-06-04 14:24:49 -0600

    Add strtobool utility

--- rcon/utils.py
+++ rcon/utils.py
@@ -520,3 +520,20 @@ class SafeStringFormat(dict):
             called_from.code_context,
         )
         return f"{{{key}}}"
+
+
+def strtobool(val) -> bool:
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+
+    # sourced from https://stackoverflow.com/a/18472142 with modification
+    val = val.lower()
+    if val in ("y", "yes", "t", "true", "on", "1"):
+        return bool(1)
+    elif val in ("n", "no", "f", "false", "off", "0"):
+        return bool(0)
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
