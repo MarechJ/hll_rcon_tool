77f9d1de1904bf487a218fd760e7970a6ed3a9c8
Jonathan Müller <52619285+LordofAgents@users.noreply.github.com>
2024-07-29 18:32:54 +0200

    Bugfix: Check if API Key is used and valid (#609)
    Allow API keys when using `stats_login_required` decorated endpoints

--- rconweb/api/auth.py
+++ rconweb/api/auth.py
@@ -195,6 +195,33 @@ def do_logout(request):
     return api_response(result=True, command="logout", failed=False)
 
 
+def check_api_key(request):
+    # Extract the header and bearer key if present, otherwise fall back on
+    # requiring the user to be logged in
+    try:
+        header_name, raw_api_key = request.META[
+            HTTP_AUTHORIZATION_HEADER
+        ].split(maxsplit=1)
+        if not header_name.upper().strip() in BEARER:
+            raw_api_key = None
+    except (KeyError, ValueError):
+        raw_api_key = None
+
+    try:
+        # If we don't include the salt, the hasher generates its own
+        # and it will generate different hashed values every time
+        hashed_api_key = make_password(
+            raw_api_key, salt=SECRET_KEY.replace("$", "")
+        )
+        api_key_model = DjangoAPIKey.objects.get(api_key=hashed_api_key)
+
+        # Retrieve the user to use the normal authentication system
+        # to include their permissions
+        request.user = api_key_model.user
+    except DjangoAPIKey.DoesNotExist:
+        pass
+
+
 def login_required():
     """Flag this endpoint as one that requires the user
     to be logged in.
@@ -203,30 +230,8 @@ def login_required():
     def decorator(func):
         @wraps(func)
         def wrapper(request, *args, **kwargs):
-            # Extract the header and bearer key if present, otherwise fall back on
-            # requiring the user to be logged in
-            try:
-                header_name, raw_api_key = request.META[
-                    HTTP_AUTHORIZATION_HEADER
-                ].split(maxsplit=1)
-                if not header_name.upper().strip() in BEARER:
-                    raw_api_key = None
-            except (KeyError, ValueError):
-                raw_api_key = None
-
-            try:
-                # If we don't include the salt, the hasher generates its own
-                # and it will generate different hashed values every time
-                hashed_api_key = make_password(
-                    raw_api_key, salt=SECRET_KEY.replace("$", "")
-                )
-                api_key_model = DjangoAPIKey.objects.get(api_key=hashed_api_key)
-
-                # Retrieve the user to use the normal authentication system
-                # to include their permissions
-                request.user = api_key_model.user
-            except DjangoAPIKey.DoesNotExist:
-                pass
+            # Check if API-Key is used
+            check_api_key(request)
 
             if not request.user.is_authenticated:
                 return api_response(
@@ -270,6 +275,10 @@ def stats_login_required(func):
 
     @wraps(func)
     def wrapper(request, *args, **kwargs):
+        # Check if API-Key is used
+        check_api_key(request)
+
+
         if not request.user.is_authenticated:
             return api_response(
                 command=request.path,
@@ -334,3 +343,4 @@ def get_own_user_permissions(request):
         },
         failed=False,
     )
+
