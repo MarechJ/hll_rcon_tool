6f413cd66e36084384f385e0706446f8154e72c5
ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>
2024-08-18 23:38:35 +0200

    Update seeding_rules.py
    Signed-off-by: ElGuillermo <66552553+ElGuillermo@users.noreply.github.com>

--- rcon/automods/seeding_rules.py
+++ rcon/automods/seeding_rules.py
@@ -1,3 +1,10 @@
+"""
+Enforces the various seeding rules :
+- disallowed roles
+- disallowed weapons
+- don't enter protected ennemy zone
+"""
+
 import logging
 import pickle
 from contextlib import contextmanager
@@ -25,17 +32,28 @@ from rcon.rcon import StructuredLogLineType
 from rcon.types import GameState, GetDetailedPlayer, Roles
 from rcon.user_config.auto_mod_seeding import AutoModSeedingUserConfig
 
+
 SEEDING_RULES_RESET_SECS = 120
 AUTOMOD_USERNAME = "SeedingRulesAutomod"
 SEEDING_RULE_NAMES = ["disallowed_roles", "disallowed_weapons", "enforce_cap_fight"]
 
 
 def disabled_rule_key(rule: str) -> str:
+    """
+    Global off switch for
+    disallowed_roles, disallowed_weapons and enforce_cap_fight rules
+    according to the min/max players parameters set for each
+    """
     return f"seeding_rules_automod_disabled_for_round_{rule}"
 
 
 @on_match_start
 def on_map_change(_, _1):
+    """
+    Resets the global off switch on map change for
+    disallowed_roles, disallowed_weapons and enforce_cap_fight rules
+    according to the min/max players parameters set for each
+    """
     keys = []
     for rule in SEEDING_RULE_NAMES:
         keys.append(disabled_rule_key(rule))
@@ -45,6 +63,9 @@ def on_map_change(_, _1):
 
 
 class SeedingRulesAutomod:
+    """
+    Imported from rcon/automods/automod.py
+    """
     logger: logging.Logger
     red: redis.StrictRedis
     config: AutoModSeedingUserConfig
@@ -59,6 +80,9 @@ class SeedingRulesAutomod:
 
 
     def enabled(self) -> bool:
+        """
+        Global on/off switch
+        """
         return self.config.enabled
 
 
@@ -68,6 +92,9 @@ class SeedingRulesAutomod:
         player_id: str,
         detailed_player_info: GetDetailedPlayer | None = None,
     ) -> PunitionsToApply:
+        """
+        Sends a one-time seeding rules reminder message to players on connect
+        """
         p: PunitionsToApply = PunitionsToApply()
 
         disallowed_roles = set(self.config.disallowed_roles.roles.values())
@@ -90,7 +117,8 @@ class SeedingRulesAutomod:
                 message = message.format(**data)
             except KeyError:
                 self.logger.warning(
-                    f"The automod message for disallowed weapons ({message}) contains an invalid key"
+                    "The automod message for disallowed weapons (%s) contains an invalid key",
+                    message
                 )
 
             author = AUTOMOD_USERNAME + ("-DryRun" if self.config.dry_run else "")
@@ -101,6 +129,7 @@ class SeedingRulesAutomod:
                     name=name,
                     squad="",
                     team="",
+                    flags=[],
                     role="",
                     lvl=0,
                     details=PunishDetails(
@@ -116,6 +145,10 @@ class SeedingRulesAutomod:
 
 
     def on_kill(self, log: StructuredLogLineType) -> PunitionsToApply:
+        """
+        Observe weapon usage
+        Punishes the trespasser if a kill using a forbidden weapon has been logged
+        """
         p: PunitionsToApply = PunitionsToApply()
 
         if log[
@@ -130,6 +163,7 @@ class SeedingRulesAutomod:
                 name=log["player_name_1"],
                 squad="",
                 team="",
+                flags=[],
                 role="",
                 lvl=0,
                 details=PunishDetails(
@@ -147,7 +181,8 @@ class SeedingRulesAutomod:
                 message = message.format(**data)
             except KeyError:
                 self.logger.warning(
-                    f"The automod message for disallowed weapons ({message}) contains an invalid key"
+                    "The automod message for disallowed weapons (%s) contains an invalid key",
+                    message
                 )
             aplayer.details.message = message
 
@@ -158,6 +193,9 @@ class SeedingRulesAutomod:
 
     @contextmanager
     def watch_state(self, team: str, squad_name: str):
+        """
+        Observe and actualize the current moderation step
+        """
         redis_key = f"seeding_rules_automod{team.lower()}{str(squad_name).lower()}"
         watch_status = self.red.get(redis_key)
         if watch_status:
@@ -169,7 +207,7 @@ class SeedingRulesAutomod:
             yield watch_status
         except NoSeedingViolation:
             self.logger.debug(
-                "Squad %s - %s no seeding violation clearing state", team, squad_name
+                "Squad %s - %s no seeding violation, clearing state", team, squad_name
             )
             self.red.delete(redis_key)
         else:
@@ -185,6 +223,10 @@ class SeedingRulesAutomod:
         violation_msg: str,
         method: ActionMethod,
     ):
+        """
+        Construct the message sent to the player
+        according to the actual moderation step
+        """
         data: dict[str, str | int] = {
             "violation": violation_msg,
         }
@@ -222,11 +264,14 @@ class SeedingRulesAutomod:
             )
             return message
 
+
     def _disable_for_round(self, rule: str):
         self.red.setex(disabled_rule_key(rule), 3 * 60 * 60, "1")
 
+
     def _enable_for_round(self, rule: str):
         self.red.delete(disabled_rule_key(rule))
+
 
     def _is_seeding_rule_disabled(self, rule: str) -> bool:
         k = disabled_rule_key(rule)
@@ -238,13 +283,19 @@ class SeedingRulesAutomod:
             v = v.decode()
         return v == "1"
 
+
     def player_punish_failed(self, aplayer):
+        """
+        A dead/unspawned player can't be punished
+        Resets the timer from the last unsuccessful punish.
+        """
         with self.watch_state(aplayer.team, aplayer.squad) as watch_status:
             try:
                 if punishes := watch_status.punished.get(aplayer.name):
                     del punishes[-1]
             except Exception:
                 self.logger.exception("tried to cleanup punished time but failed")
+
 
     def punitions_to_apply(
         self,
@@ -254,10 +305,18 @@ class SeedingRulesAutomod:
         squad: dict,
         game_state: GameState,
     ) -> PunitionsToApply:
+        """
+        Observe all squads/players
+        Find the ones who trespass rules
+        Sends them to their next moderation step
+        """
         self.logger.debug("Squad %s %s", squad_name, squad)
         punitions_to_apply = PunitionsToApply()
 
-        server_player_count = get_team_count(team_view, "allies") + get_team_count(team_view, "axis")
+        server_player_count = (
+            get_team_count(team_view, "allies")
+            + get_team_count(team_view, "axis")
+        )
 
         # dont_do_anything_below_this_number_of_players
         if server_player_count < self.config.dont_do_anything_below_this_number_of_players:
@@ -265,13 +324,13 @@ class SeedingRulesAutomod:
             return punitions_to_apply
 
         if not squad_name:
-            self.logger.debug("Skipping None or empty squad %s %s", squad_name, squad)
+            self.logger.debug("Skipping None or empty squad - (%s) %s", team, squad_name)
             return punitions_to_apply
 
         with self.watch_state(team, squad_name) as watch_status:
 
-            if squad_name is None or squad is None:
-                raise NoSeedingViolation()
+            # if squad_name is None or squad is None:
+            #     raise NoSeedingViolation()
 
             author = AUTOMOD_USERNAME + ("-DryRun" if self.config.dry_run else "")
 
@@ -281,7 +340,6 @@ class SeedingRulesAutomod:
                     name=player["name"],
                     squad=squad_name,
                     team=team,
-                    # flags=player.get('profile', {}).get('flags', []),
                     flags=player.get('profile', {}).get('flags', []),
                     role=player.get("role"),
                     lvl=int(player.get("level")),
@@ -319,13 +377,6 @@ class SeedingRulesAutomod:
                 else:
                     self._enable_for_round("enforce_cap_fight")
 
-                # whitelist_flags
-                if any(
-                    flag_entry.flag in self.config.whitelist_flags
-                    for flag_entry in aplayer.flags
-                ):
-                    continue
-
                 violations = []
 
                 if (
@@ -347,15 +398,14 @@ class SeedingRulesAutomod:
                     (team == "axis" and game_state["axis_score"] >= ecf.max_caps)
                     or (team == "allies" and game_state["allied_score"] >= ecf.max_caps)
                 ):
-                    self.logger.debug("Player is on " + team + " side and winning")
+                    self.logger.debug("Player is on %s side and winning", team)
                     op = player["offense"]
                     oop = watch_status.offensive_points.setdefault(aplayer.name, -1)
 
                     self.logger.debug(
-                        "Player had "
-                        + str(oop)
-                        + " offensive points last and now "
-                        + str(op)
+                        "Player had %s offensive points last and now %s",
+                        str(oop),
+                        str(op)
                     )
                     if oop != -1 and oop < op:
                         violations.append(ecf.violation_message)
@@ -378,6 +428,9 @@ class SeedingRulesAutomod:
 
                 violation_msg = ", ".join(violations)
 
+                # Note (not applicable)
+
+                # Warning
                 state = self.should_warn_player(
                     watch_status, squad_name, aplayer
                 )
@@ -399,6 +452,7 @@ class SeedingRulesAutomod:
                 ]:
                     continue
 
+                # Punish
                 state = self.should_punish_player(
                     watch_status, team_view, squad_name, squad, aplayer
                 )
@@ -416,6 +470,7 @@ class SeedingRulesAutomod:
                 ]:
                     continue
 
+                # Kick
                 state = self.should_kick_player(
                     watch_status, team_view, squad_name, squad, aplayer
                 )
@@ -436,16 +491,33 @@ class SeedingRulesAutomod:
         return punitions_to_apply
 
 
+    # (not applicable)
+    # def should_note_player()
+
+
     def should_warn_player(
         self, watch_status: WatchStatus, squad_name: str, aplayer: PunishPlayer
     ):
+        """
+        Send a message to trespassers
+        telling them they must follow the rules
+        before being punished and kicked
+        """
         # number_of_warnings
         if self.config.number_of_warnings == 0:
             self.logger.debug("Warnings are disabled. number_of_warning is set to 0")
             return PunishStepState.DISABLED
 
-        # immune_player_level
+        # whitelist_flags
+        if any(
+            flag_entry.flag in self.config.whitelist_flags
+            for flag_entry in aplayer.flags
+        ):
+            self.logger.debug("%s is immune to warnings", aplayer.short_repr())
+            return PunishStepState.IMMUNED
+
         # immune_roles
+        # immune_player_level
         if (
             aplayer.lvl <= self.config.immune_player_level
             or aplayer.role in self.config.immune_roles
@@ -484,6 +556,7 @@ class SeedingRulesAutomod:
         )
         return PunishStepState.GO_TO_NEXT_STEP
 
+
     def should_punish_player(
         self,
         watch_status: WatchStatus,
@@ -492,10 +565,37 @@ class SeedingRulesAutomod:
         squad,
         aplayer: PunishPlayer,
     ):
+        """
+        Punish (kill) trespassers
+        telling them they must follow the rules
+        before being kicked
+        """
         # number_of_punishments
         if self.config.number_of_punishments == 0:
             self.logger.debug("Punish is disabled")
             return PunishStepState.DISABLED
+
+        # whitelist_flags
+        if any(
+            flag_entry.flag in self.config.whitelist_flags
+            for flag_entry in aplayer.flags
+        ):
+            self.logger.debug("%s is immune to punishment", aplayer.short_repr())
+            return PunishStepState.IMMUNED
+
+        # immune_roles
+        # immune_player_level
+        if (
+            aplayer.lvl <= self.config.immune_player_level
+            or aplayer.role in self.config.immune_roles
+        ):
+            self.logger.debug("%s is immune to punishment", aplayer.short_repr())
+            return PunishStepState.IMMUNED
+
+        # min_squad_players_for_punish
+        if len(squad["players"]) < self.config.min_squad_players_for_punish:
+            self.logger.debug("Squad %s below min player count for punish", squad_name)
+            return PunishStepState.WAIT
 
         # min_server_players_for_punish
         if (
@@ -503,20 +603,6 @@ class SeedingRulesAutomod:
         ) < self.config.min_server_players_for_punish:
             self.logger.debug("Server below min player count for punish")
             return PunishStepState.WAIT
-
-        # min_squad_players_for_punish
-        if len(squad["players"]) < self.config.min_squad_players_for_punish:
-            self.logger.debug("Squad %s below min player count for punish", squad_name)
-            return PunishStepState.WAIT
-
-        # immune_player_level
-        # immune_roles
-        if (
-            aplayer.lvl <= self.config.immune_player_level
-            or aplayer.role in self.config.immune_roles
-        ):
-            self.logger.debug("%s is immune to punishment", aplayer.short_repr())
-            return PunishStepState.IMMUNED
 
         punishes = watch_status.punished.setdefault(aplayer.name, [])
 
@@ -547,6 +633,7 @@ class SeedingRulesAutomod:
         )
         return PunishStepState.GO_TO_NEXT_STEP
 
+
     def should_kick_player(
         self,
         watch_status: WatchStatus,
@@ -555,10 +642,36 @@ class SeedingRulesAutomod:
         squad,
         aplayer: PunishPlayer,
     ):
+        """
+        Kick (disconnect) trespassers
+        telling them they must follow the rules
+        """
         # kick_after_max_punish
         if not self.config.kick_after_max_punish:
             self.logger.debug("Kick is disabled")
             return PunishStepState.DISABLED
+
+        # whitelist_flags
+        if any(
+            flag_entry.flag in self.config.whitelist_flags
+            for flag_entry in aplayer.flags
+        ):
+            self.logger.debug("%s is immune to kick", aplayer.short_repr())
+            return PunishStepState.IMMUNED
+
+        # immune_roles
+        # immune_player_level
+        if (
+            aplayer.lvl <= self.config.immune_player_level
+            or aplayer.role in self.config.immune_roles
+        ):
+            self.logger.debug("%s is immune to kick", aplayer.short_repr())
+            return PunishStepState.IMMUNED
+
+        # min_squad_players_for_kick
+        if len(squad["players"]) < self.config.min_squad_players_for_kick:
+            self.logger.debug("Squad %s below min player count for punish", squad_name)
+            return PunishStepState.WAIT
 
         # min_server_players_for_kick
         if (
@@ -566,20 +679,6 @@ class SeedingRulesAutomod:
         ) < self.config.min_server_players_for_kick:
             self.logger.debug("Server below min player count for punish")
             return PunishStepState.WAIT
-
-        # min_squad_players_for_kick
-        if len(squad["players"]) < self.config.min_squad_players_for_kick:
-            self.logger.debug("Squad %s below min player count for punish", squad_name)
-            return PunishStepState.WAIT
-
-        # immune_player_level
-        # immune_roles
-        if (
-            aplayer.lvl <= self.config.immune_player_level
-            or aplayer.role in self.config.immune_roles
-        ):
-            self.logger.debug("%s is immune to kick", aplayer.short_repr())
-            return PunishStepState.IMMUNED
 
         try:
             last_time = watch_status.punished.get(aplayer.name, [])[-1]
