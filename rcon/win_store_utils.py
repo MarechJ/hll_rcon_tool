3ddea284f21381f593872677f77f39de7c42b293
C. Eric Mathey <emathey@protonmail.com>
2024-09-18 13:01:13 -0600

    Simplify check for non steam player IDs

--- rcon/win_store_utils.py
+++ rcon/win_store_utils.py
@@ -1,16 +1,9 @@
 import re
 from logging import getLogger
 from typing import Final
+from rcon.steam_utils import is_steam_id_64
 
 logger = getLogger(__name__)
 
-WIN_STORE_PLAYER_ID_REGEX: Final = (
-    "^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$"
-)
-win_store_regexp = re.compile(WIN_STORE_PLAYER_ID_REGEX)
-
-
 def is_windows_store_id(player_id: str) -> bool:
-    if win_store_regexp.match(player_id):
-        return True
-    return False
+    return not is_steam_id_64(player_id=player_id)
