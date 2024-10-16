2d6c289f925b33ba5cc02efb65074612b9bff5d2
Tim Raaymakers <43066279+timraay@users.noreply.github.com>
2024-10-16 17:51:45 +0200

    Add U15.3 maps and commands (#727)

--- rcon/commands.py
+++ rcon/commands.py
@@ -754,6 +754,12 @@ class ServerCtl:
             can_fail=False
         )
         return list(objectives)
+    
+    def get_game_mode(self):
+        """
+        Any of "IntenseWarfare", "OffensiveWarfare", or ???
+        """
+        return self._str_request("get gamemode", can_fail=False)
 
 if __name__ == "__main__":
     from rcon.settings import SERVER_INFO
