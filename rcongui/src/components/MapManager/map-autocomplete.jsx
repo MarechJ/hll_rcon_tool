11b1da2a2fca8757ddb8757f17e1d55eee41c7c1
Dorfieeee <rosendorf.dev@gmail.com>
2024-11-12 21:42:52 +0000

    fix map change, fix map pages errors, list item & menu button styles

--- rcongui/src/components/MapManager/map-autocomplete.jsx
+++ rcongui/src/components/MapManager/map-autocomplete.jsx
@@ -1,5 +1,5 @@
 import { TextField } from "@mui/material";
-import { Autocomplete } from '@mui/material';
+import { Autocomplete } from "@mui/material";
 import { MapDetail } from "./map-details";
 
 export function MapAutocomplete({ options, onChange, ...props }) {
@@ -17,8 +17,9 @@ export function MapAutocomplete({ options, onChange, ...props }) {
         <TextField {...params} variant="outlined" label="Select maps" />
       )}
       renderOption={(props, option) => {
+        const { key, ...optionProps } = props;
         return (
-          <li {...props}>
+          <li key={key} {...optionProps}>
             <MapDetail mapLayer={option} />
           </li>
         );
--- rcongui/src/pages/settings/map-manager/votemap/vote-status.jsx
+++ rcongui/src/pages/settings/map-manager/votemap/vote-status.jsx
@@ -1,24 +1,28 @@
 import * as React from "react";
-import { List, Typography } from "@mui/material";
+import { List, Typography, Divider } from "@mui/material";
 import { MapListItem } from "@/components/MapManager/map-list-item";
 
 export function VoteStatus({ voteStatus, ...props }) {
   return voteStatus.length ? (
     <List dense={true} {...props}>
-      {voteStatus.map((mapStatus, index) => {
+      {voteStatus.map((mapStatus, index, array) => {
         const { map, voters } = mapStatus;
         return (
-          <MapListItem
-            key={`${index}#${map.id}`}
-            mapLayer={map}
-            primary={`#${index + 1} ${map.map.pretty_name} -- ${
-              voters.length
-            } vote${voters.length !== 1 ? "s" : ""}`}
-          />
+          <React.Fragment key={`${index}#${map.id}`}>
+            {index !== 0 && <Divider flexItem variant="inset" />}
+            <MapListItem
+              mapLayer={map}
+              primary={`#${index + 1} ${map.map.pretty_name} -- ${
+                voters.length
+              } vote${voters.length !== 1 ? "s" : ""}`}
+            />
+          </React.Fragment>
         );
       })}
     </List>
   ) : (
-    <Typography variant="h4" component={"div"} style={{ padding: 8 }}>No VoteMap History</Typography>
+    <Typography variant="h4" component={"div"} style={{ padding: 8 }}>
+      No VoteMap History
+    </Typography>
   );
 }
