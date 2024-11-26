861ddee521398a5ba7730c57431605c52853a4b0
Florian <florian.schmidt.welzow@t-online.de>
2024-11-27 00:08:51 +0100

    update migrations

--- rconweb/api/migrations/0016_alter_steamplayer_steam_id_64.py
+++ rconweb/api/migrations/0016_alter_steamplayer_steam_id_64.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("api", "0014_alter_rconuser_options"),
+        ("api", "0015_alter_rconuser_options"),
     ]
 
     operations = [
