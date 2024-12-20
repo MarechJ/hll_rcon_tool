cdbda68fd8d9d30e3ea54ce80981b78d9656c5ba
Dorfieeee <rosendorf.dev@gmail.com>
2024-12-20 12:49:06 +0000

    added console admin action

---
+++ rcongui/src/features/player-action/forms/AddConsoleAdminFormFields.jsx
@@ -0,0 +1,31 @@
+import { ControlledSelect } from "@/components/form/core/ControlledSelect";
+import { Stack } from "@mui/material";
+import { useEffect } from "react";
+
+export const AddConsoleAdminFormFields = ({
+  contextData,
+  control,
+  setError,
+  contextError,
+}) => {
+  useEffect(() => {
+    if (contextError) {
+      setError("role", { message: contextError.message });
+    }
+  }, [contextError]);
+
+  return (
+    <Stack spacing={2}>
+      <ControlledSelect
+        control={control}
+        name="role"
+        label="Admin Groups"
+        rules={{ required: true }}
+        options={contextData?.admin_groups?.map((groupName) => ({
+          label: groupName,
+          value: groupName,
+        }))}
+      />
+    </Stack>
+  );
+};
