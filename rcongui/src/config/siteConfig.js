1cc4648c535ef2e09a44278cfaa27074c5e55015
Dorfieeee <rosendorf.dev@gmail.com>
2024-12-28 20:18:17 +0000

    error element handling http and general errors

--- rcongui/src/config/siteConfig.js
+++ rcongui/src/config/siteConfig.js
@@ -2,4 +2,5 @@ export default {
     appName: 'crcon',
     repoOwner: 'MarechJ',
     repoName: 'hll_rcon_tool',
+    bugReportChannelUrl: 'https://discord.com/channels/685692524442026020/706075642885832764',
 }
