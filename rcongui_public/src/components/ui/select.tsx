971713e4423e0b43a1e470ceaeb4f976f66d35db
marcelhillesheim <contact@marcelhillesheim.com>
2024-12-30 14:17:26 +0100

    FE: fixed: added missing hover effect for select

--- rcongui_public/src/components/ui/select.tsx
+++ rcongui_public/src/components/ui/select.tsx
@@ -2,9 +2,9 @@
 
 import * as React from 'react'
 import * as SelectPrimitive from '@radix-ui/react-select'
-import { Check, ChevronDown, ChevronUp } from 'lucide-react'
+import {Check, ChevronDown, ChevronUp} from 'lucide-react'
 
-import { cn } from '@/lib/utils'
+import {cn} from '@/lib/utils'
 
 const Select = SelectPrimitive.Root
 
@@ -19,7 +19,7 @@ const SelectTrigger = React.forwardRef<
   <SelectPrimitive.Trigger
     ref={ref}
     className={cn(
-      'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1',
+      'flex h-10 w-full items-center justify-between rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 hover:bg-accent',
       className,
     )}
     {...props}
