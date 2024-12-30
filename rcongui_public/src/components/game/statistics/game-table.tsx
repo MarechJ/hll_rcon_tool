1d8dc81575449cc4ea296b30270fa98a54c665e4
marcelhillesheim <contact@marcelhillesheim.com>
2024-12-30 14:27:17 +0100

    FE: display count for team selection

--- rcongui_public/src/components/game/statistics/game-table.tsx
+++ rcongui_public/src/components/game/statistics/game-table.tsx
@@ -12,7 +12,7 @@ import {
 } from '@tanstack/react-table'
 
 import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from '@/components/ui/table'
-import React, {Fragment, useState} from 'react'
+import React, {useEffect, useMemo, useState} from 'react'
 
 import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from '@/components/ui/select'
 
@@ -21,10 +21,10 @@ import {useTranslation} from 'react-i18next'
 import {Button} from '@/components/ui/button'
 import {Download} from 'lucide-react'
 import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from '@/components/ui/tooltip'
+import {getTeamFromAssociation} from "@/components/game/statistics/utils";
 import useGameDownload from '@/hooks/use-game-download'
-import TagList from "@/components/tag-list";
-import {Input} from "@/components/ui/input";
 import {TeamIndicator} from "@/components/game/statistics/team-indicator";
+import SelectBox from "@/components/ui/select-box";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[]
@@ -65,6 +65,7 @@ export function DataTable<TData extends Player, TValue>({columns, data, tableId}
   const hasIsOnline = table.getAllColumns().find((c) => c.id === 'is_online')
   const hasTeam = table.getAllColumns().find((c) => c.id === 'team')
   const teamOptions = ["axis", "allies", "mixed", "unknown"] as const;
+  const teamCounts = useMemo(() => teamOptions.map(team => data.filter(player => getTeamFromAssociation(player.team) === team).length), [data]);
 
   return (
     <div className="border w-full divide-y">
@@ -88,12 +89,16 @@ export function DataTable<TData extends Player, TValue>({columns, data, tableId}
                 <SelectValue placeholder={t('playersTable.team')}/>
               </SelectTrigger>
               <SelectContent>
-                <SelectItem value="all">{t('onlineStatusFilter.all')}</SelectItem>
-                {teamOptions.map(option =>
+                <SelectItem value="all">
+                  <div className={"pl-5"}>
+                    {t('onlineStatusFilter.all')} ({data.length})
+                  </div>
+                </SelectItem>
+                {teamOptions.map((option, index) =>
                   <SelectItem value={option}>
                     <div className="flex">
                       <TeamIndicator team={option as TeamEnum} className="block m-auto"/>
-                      <div className="pl-3">{t(option)}</div>
+                      <div className="pl-3">{t(option)} ({teamCounts[index]})</div>
                     </div>
                   </SelectItem>
                 )}
