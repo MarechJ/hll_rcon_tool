093a56e7a4f397da177960ce5aa0eca8c0b975fb
marcelhillesheim <contact@marcelhillesheim.com>
2025-01-10 10:21:34 +0100

    FE: Stats: fixed: rounded css

--- rcongui_public/src/components/game/statistics/team-indicator.tsx
+++ rcongui_public/src/components/game/statistics/team-indicator.tsx
@@ -8,13 +8,13 @@ type TeamIndicatorProps = {
 
 export function TeamIndicator({ team, ...props }: TeamIndicatorProps) {
   if (team === TeamEnum.AXIS) {
-    return <span className={cn('w-4 size-2 rounded bg-red-600', props.className)}></span>
+    return <span className={cn('size-2 rounded-full bg-red-600', props.className)}></span>
   } else if (team === TeamEnum.ALLIES) {
-    return <span className={cn('w-4 size-2 rounded bg-blue-600', props.className)}></span>
+    return <span className={cn('size-2 rounded-full bg-blue-600', props.className)}></span>
   } else if (team === TeamEnum.MIXED) {
-    return <span className={cn('w-4 size-2 rounded bg-yellow-400', props.className)}></span>
+    return <span className={cn('size-2 rounded-full bg-yellow-400', props.className)}></span>
   } else if (team === TeamEnum.UNKNOWN) {
-    return <span className={cn('w-4 size-2 rounded bg-gray-500', props.className)}></span>
+    return <span className={cn('size-2 rounded-full bg-gray-500', props.className)}></span>
   }
   return null;
 }
