8fcefe6fddd75e3ae804737722374b4b4e244777
marcelhillesheim <contact@marcelhillesheim.com>
2025-01-16 13:20:21 +0100

    FE: refactored game duration util

--- rcongui_public/src/pages/games/utils.ts
+++ rcongui_public/src/pages/games/utils.ts
@@ -1,16 +1,7 @@
 import dayjs from 'dayjs'
 
 export function getGameDuration(start: string, end: string) {
-  const totalSeconds = dayjs(end).diff(dayjs(start), 'seconds')
-  const hours = Math.floor(totalSeconds / 3600)
-  const minutes = Math.floor((totalSeconds % 3600) / 60)
-  const seconds = totalSeconds % 60
+  const duration = dayjs.duration(dayjs(end).diff(dayjs(start)));
 
-  // Format the result as hh:mm:ss
-  const formattedTime = `${String(hours).padStart(1, '0')}:${String(minutes).padStart(
-    2,
-    '0',
-  )}:${String(seconds).padStart(2, '0')}`
-
-  return formattedTime
+  return duration.format('H:mm:ss');
 }
