e0ab06eb1b9e7bce58116bfdc7383b38a311c6a0
marcelhillesheim <contact@marcelhillesheim.com>
2025-02-03 08:45:34 +0100

    FE: Streaming: store stream settings in local storage

--- rcongui_public/src/pages/home/streaming/index.tsx
+++ rcongui_public/src/pages/home/streaming/index.tsx
@@ -1,6 +1,6 @@
 import {Slider} from "@/components/ui/slider";
-import React, {useState} from "react";
-import StreamBanner from "@/pages/home/streaming/stream-banner";
+import React from "react";
+import StreamBanner, {StreamSettings} from "@/pages/home/streaming/stream-banner";
 import {Input} from "@/components/ui/input";
 import {Switch} from "@/components/ui/switch";
 import {useTranslation} from "react-i18next";
@@ -16,25 +16,27 @@ import {
 import {AccordionHeader} from "@radix-ui/react-accordion";
 import {GraduationCap} from "lucide-react";
 import StreamTutorial from "@/pages/home/streaming/stream-tutorial";
+import {useStorageState} from "@/hooks/use-storage-state";
 
 export default function Streaming() {
   const { t } = useTranslation('translation', {keyPrefix: 'streaming'});
 
   const { liveStats } = useOutletContext<GameLiveOutletContext>();
 
-  const [playerFilter, setPlayerFilter] = useState<string[]>([]);
-
-  const [playerAmount, setPlayerAmount] = useState<number>(5);
-  const [animationDuration, setAnimationDuration] = useState<number>(15);
-  const [pauseWidth, setPauseWidth] = useState<number>(150);
-  const [text, setText] = useState<string>('TOP KILLS:');
-  const [showAvatars, setShowAvatars] = useState(false);
-  const [showWeapons, setShowWeapons] = useState(false);
+  const [streamSettings, setStreamSettings] = useStorageState<StreamSettings>('stream-settings', {
+    playerFilter: [],
+    playerAmount: 5,
+    animationDuration: 15,
+    pauseWidth: 150,
+    text: 'TOP KILLS:',
+    showAvatars: false,
+    showWeapons: false,
+  })
 
   const playerFilterOptions = liveStats.data
     .map((player) => ({ value: player.player, label: player.player }))
     .concat(
-      playerFilter
+      streamSettings.playerFilter
         .filter((name) => !liveStats.data.some((player) => player.player === name))
         .map((name) => ({ value: name, label: name })),
     );
@@ -57,56 +59,55 @@ export default function Streaming() {
     <div className="bg-background w-full border-x border-b gap-3 p-2 flex flex-wrap">
       <div className="w-52 h-12 flex flex-col justify-between pb-2">
         <span className="text-nowrap">
-          {t('playerCount')} ({playerAmount})
+          {t('playerCount')} ({streamSettings.playerAmount})
         </span>
-        <Slider onValueChange={value => setPlayerAmount(value[0])}
-                value={[playerAmount]}
+        <Slider onValueChange={value => setStreamSettings(prev => ({...prev, playerAmount: value[0]}))}
+                value={[streamSettings.playerAmount]}
                 max={25} step={1}/>
       </div>
       <div className="w-52 h-12 flex flex-col justify-between pb-2">
         <span className="text-nowrap">
-          {t('cycleDuration')} ({animationDuration}s)
+          {t('cycleDuration')} ({streamSettings.animationDuration}s)
         </span>
-        <Slider onValueChange={value => setAnimationDuration(value[0])}
-                value={[animationDuration]}
+        <Slider onValueChange={value => setStreamSettings(prev => ({...prev, animationDuration: value[0]}))}
+                value={[streamSettings.animationDuration]}
                 max={60} step={1}/>
       </div>
       <div className="w-52 h-12 flex flex-col justify-between pb-2">
         <span className="text-nowrap">
-          {t('pauseWidth')} ({pauseWidth})
+          {t('pauseWidth')} ({streamSettings.pauseWidth})
         </span>
-        <Slider onValueChange={value => setPauseWidth(value[0])}
-                value={[pauseWidth]}
+        <Slider onValueChange={value => setStreamSettings(prev => ({...prev, pauseWidth: value[0]}))}
+                value={[streamSettings.pauseWidth]}
                 max={1000} step={10}/>
       </div>
       <div className="w-52 text-nowrap flex-1">
-        <Input placeholder={t('text')} value={text} onChange={(e) => setText(e.target.value)}/>
+        <Input placeholder={t('text')} value={streamSettings.text} onChange={(e) => setStreamSettings(prev => ({...prev, text: e.target.value}))}/>
       </div>
       <div className="w-52 h-10 text-nowrap items-center flex">
         <span className="pr-4">
           {t('showAvatars')}:
         </span>
-        <Switch checked={showAvatars} onCheckedChange={setShowAvatars}/>
+        <Switch checked={streamSettings.showAvatars} onCheckedChange={(value) => setStreamSettings(prev => ({...prev, showAvatars: value}))}/>
       </div>
       <div className="w-52 h-10 text-nowrap items-center flex">
         <span className="pr-4">
           {t('showWeapons')}:
         </span>
-        <Switch checked={showWeapons} onCheckedChange={setShowWeapons}/>
+        <Switch checked={streamSettings.showWeapons} onCheckedChange={(value) => setStreamSettings(prev => ({...prev, showWeapons: value}))}/>
       </div>
       <div className="flex-1">
         <SelectBox
           options={playerFilterOptions}
           multiple
-          value={playerFilter}
-          onChange={(values) => Array.isArray(values) && setPlayerFilter(values)}
+          value={streamSettings.playerFilter}
+          onChange={(values) => Array.isArray(values) && setStreamSettings(prev => ({...prev, playerFilter: values}))}
           placeholder={`${t('hidePlayer')}...`}
         />
       </div>
     </div>
     <div className="w-full border-x border-b gap-3 p-2">
-      <StreamBanner playerAmount={playerAmount}
-                    settings={{animationDuration, text, showAvatars, showWeapons, pauseWidth, playerFilter}}/>
+      <StreamBanner settings={streamSettings}/>
     </div>
   </div>
 }
