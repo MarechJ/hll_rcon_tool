586134cae8923ea31b2d3731ac3f847b29eeebcc
C. Eric Mathey <emathey@protonmail.com>
2025-02-14 19:08:00 -0700

    Include player ID when editing a profile

--- rcongui/src/components/VipList/VipListRecordGrid.js
+++ rcongui/src/components/VipList/VipListRecordGrid.js
@@ -19,10 +19,10 @@ import FileCopyIcon from "@mui/icons-material/FileCopy";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import StarIcon from "@mui/icons-material/Star";
-import StarBorderIcon from '@mui/icons-material/StarBorder';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import ToggleOnIcon from '@mui/icons-material/ToggleOn';
-import ToggleOffIcon from '@mui/icons-material/ToggleOff';
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
+import ToggleOnIcon from "@mui/icons-material/ToggleOn";
+import ToggleOffIcon from "@mui/icons-material/ToggleOff";
 import AnnouncementIcon from "@mui/icons-material/Announcement";
 import makePlayerProfileUrl from "../../utils/makePlayerProfileUrl";
 import { getCountry } from "../PlayersHistory/PlayerTile/PlayerHeader";
@@ -41,13 +41,17 @@ const VipListRecordTile = ({
   onDelete,
 }) => {
   const [showAll, setShowAll] = useState(false);
-  const expiresAt = record.get("expires_at") ? moment(record.get("expires_at")) : null
-  const isActive = record.get("is_active")
+  const expiresAt = record.get("expires_at")
+    ? moment(record.get("expires_at"))
+    : null;
+  const isActive = record.get("is_active");
   const player = record.get("player");
-  const email = record.get("player").get('email');
-  const discordId = record.get("player").get('discord_id');
+  const email = record.get("player").get("email");
+  const discordId = record.get("player").get("discord_id");
   const playerNames = player.get("names", List());
-  const firstName = playerNames.get(0) ? playerNames.get(0).get("name") : undefined;
+  const firstName = playerNames.get(0)
+    ? playerNames.get(0).get("name")
+    : undefined;
   const firstNameLetter = (firstName || "?")[0];
   const hasMultipleNames = playerNames.size > 1;
 
@@ -59,28 +63,36 @@ const VipListRecordTile = ({
     ? player.get("steaminfo").get("country", "")
     : "";
 
-  const description = record.get('description')
-  const notes = record.get('notes')
+  const description = record.get("description");
+  const notes = record.get("notes");
   const createdAt = moment(record.get("created_at"));
   const now = moment();
 
   return (
-    (<Grid
+    <Grid
       container
-      style={isActive ? { background: "linear-gradient(rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05))" } : {}}
+      style={
+        isActive
+          ? {
+              background:
+                "linear-gradient(rgba(0, 0, 0, 0.05), rgba(0, 0, 0, 0.05))",
+            }
+          : {}
+      }
       direction="column"
       justifyContent="space-between"
     >
       <MaterialList>
-        <ListItem component={"div"} alignItems="flex-start" style={{ paddingBottom: 0 }}>
+        <ListItem
+          component={"div"}
+          alignItems="flex-start"
+          style={{ paddingBottom: 0 }}
+        >
           <ListItemAvatar>
             <Link
               target="_blank"
               color="inherit"
-              href={makePlayerProfileUrl(
-                player.get("player_id"),
-                firstName
-              )}
+              href={makePlayerProfileUrl(player.get("player_id"), firstName)}
             >
               <Avatar src={avatarUrl}>{firstNameLetter}</Avatar>
             </Link>
@@ -132,7 +144,7 @@ const VipListRecordTile = ({
             }
           />
           <ListItemIcon>
-            {record.get('is_active') ? <ToggleOnIcon /> : <ToggleOffIcon />}
+            {record.get("is_active") ? <ToggleOnIcon /> : <ToggleOffIcon />}
           </ListItemIcon>
           <ListItemSecondaryAction>
             <Tooltip title="Copy Player ID to clipboard">
@@ -148,7 +160,9 @@ const VipListRecordTile = ({
                     let text = player.get("player_id");
                     navigator.clipboard.writeText(text).then(
                       function () {
-                        console.log("Async: Copying to clipboard was successful!");
+                        console.log(
+                          "Async: Copying to clipboard was successful!"
+                        );
                       },
                       function (err) {
                         console.error("Async: Could not copy text: ", err);
@@ -166,48 +180,42 @@ const VipListRecordTile = ({
       <Typography variant={description?.length > 65 ? "body2" : "body1"}>
         {description?.length > 110
           ? description?.substring(0, 108) + "..."
-          : description
-        }
+          : description}
       </Typography>
       <Typography variant={notes?.length > 65 ? "body2" : "body1"}>
-        {notes?.length > 110
-          ? notes?.substring(0, 108) + "..."
-          : notes
-        }
+        {notes?.length > 110 ? notes?.substring(0, 108) + "..." : notes}
       </Typography>
       {email ? <Typography variant="body1">{email}</Typography> : ""}
       {discordId ? <Typography variant="body1">{discordId}</Typography> : ""}
 
-      <Grid
-        container
-        justifyContent="space-between"
-        spacing={0}
-      >
-        <Grid
-          container
-          justifyContent="space-around"
-          spacing={0}
-        >
+      <Grid container justifyContent="space-between" spacing={0}>
+        <Grid container justifyContent="space-around" spacing={0}>
           <Grid>
-            <Tooltip title={expiresAt ? expiresAt.format("LLLL") : "Permanent"} arrow>
+            <Tooltip
+              title={expiresAt ? expiresAt.format("LLLL") : "Permanent"}
+              arrow
+            >
               <small>
                 {expiresAt
-                  ? (
-                    now.isBefore(expiresAt)
-                      ? `Expires in ${moment.duration(now.diff(expiresAt)).humanize()} `
-                      : `Expired ${moment.duration(now.diff(expiresAt)).humanize()} ago `
-                  )
-                  : `Never expires `
-                }
+                  ? now.isBefore(expiresAt)
+                    ? `Expires in ${moment
+                        .duration(now.diff(expiresAt))
+                        .humanize()} `
+                    : `Expired ${moment
+                        .duration(now.diff(expiresAt))
+                        .humanize()} ago `
+                  : `Never expires `}
               </small>
             </Tooltip>
           </Grid>
         </Grid>
         <Grid container justifyContent="space-around" spacing={0}>
           <Grid>
-            <Tooltip title='Admin' arrow>
+            <Tooltip title="Admin" arrow>
               <small>
-                {record.get("admin_name") ? `by ${record.get("admin_name")}` : ""}
+                {record.get("admin_name")
+                  ? `by ${record.get("admin_name")}`
+                  : ""}
               </small>
             </Tooltip>
           </Grid>
@@ -224,15 +232,11 @@ const VipListRecordTile = ({
           onDelete={() => onDelete(record)}
         />
       </Grid>
-    </Grid>)
+    </Grid>
   );
-}
+};
 
-const VipListRecordGrid = ({
-  vipLists,
-  records,
-  onRefresh,
-}) => {
+const VipListRecordGrid = ({ vipLists, records, onRefresh }) => {
   const [editDialogOpen, setEditDialogOpen] = useState(false);
   const [editDialogInitialValues, setEditDialogInitialValues] = useState();
 
@@ -245,9 +249,9 @@ const VipListRecordGrid = ({
       active: record.get("active"),
       expiresAt: record.get("expires_at"),
       notes: record.get("notes"),
-      email: record.get('player').get('email'),
-      discordId: record.get('player').get('discord_id'),
-    })
+      email: record.get("player").get("email"),
+      discordId: record.get("player").get("discord_id"),
+    });
     setEditDialogOpen(true);
   }
 
@@ -264,24 +268,28 @@ const VipListRecordGrid = ({
       .then((response) =>
         showResponse(response, `Record ${recordId} was edited`, true)
       )
-      .then(result => result && !result.failed && onRefresh())
-      .catch(handle_http_errors)
+      .then((result) => result && !result.failed && onRefresh())
+      .catch(handle_http_errors);
 
     /* TODO: this should probably be done separately */
-    let payload = {}
+    let payload = {};
     if (data.email) {
-      payload.email = data.email
+      payload.email = data.email;
     }
     if (data.discordId) {
-      payload.discord_id = data.discordId
+      payload.discord_id = data.discordId;
     }
 
     if (Object.keys(payload).length > 0) {
-      postData(`${process.env.REACT_APP_API_URL}update_player_profile`, payload).then((response) =>
-        showResponse(response, `Player profile updated`, true)
-      )
-        .then(result => result && !result.failed && onRefresh())
-        .catch(handle_http_errors)
+      postData(`${process.env.REACT_APP_API_URL}update_player_profile`, {
+        player_id: data.playerId,
+        ...payload,
+      })
+        .then((response) =>
+          showResponse(response, `Player profile updated`, true)
+        )
+        .then((result) => result && !result.failed && onRefresh())
+        .catch(handle_http_errors);
     }
   }
 
@@ -293,8 +301,8 @@ const VipListRecordGrid = ({
       .then((response) =>
         showResponse(response, `Record ${record.get("id")} was activated`, true)
       )
-      .then(result => result && !result.failed && onRefresh())
-      .catch(handle_http_errors)
+      .then((result) => result && !result.failed && onRefresh())
+      .catch(handle_http_errors);
   }
 
   function onInactivateRecord(record) {
@@ -303,10 +311,14 @@ const VipListRecordGrid = ({
       active: false,
     })
       .then((response) =>
-        showResponse(response, `Record ${record.get("id")} was deactivated`, true)
+        showResponse(
+          response,
+          `Record ${record.get("id")} was deactivated`,
+          true
+        )
       )
-      .then(result => result && !result.failed && onRefresh())
-      .catch(handle_http_errors)
+      .then((result) => result && !result.failed && onRefresh())
+      .catch(handle_http_errors);
   }
 
   function onDeleteRecord(record) {
@@ -316,8 +328,8 @@ const VipListRecordGrid = ({
       .then((response) =>
         showResponse(response, `Record ${record.get("id")} was deleted`, true)
       )
-      .then(result => result && !result.failed && onRefresh())
-      .catch(handle_http_errors)
+      .then((result) => result && !result.failed && onRefresh())
+      .catch(handle_http_errors);
   }
 
   const size = {
@@ -329,15 +341,13 @@ const VipListRecordGrid = ({
   }[2];
 
   return (
-    (<Fragment>
+    <Fragment>
       <Grid container>
         <Grid size={12}>
           <ImageList cols={size} cellHeight={210} spacing={12}>
             {records.map((record) => {
               return (
-                <ImageListItem
-                  key={record.get("id")}
-                >
+                <ImageListItem key={record.get("id")}>
                   <VipListRecordTile
                     record={record}
                     onEdit={onEditRecord}
@@ -361,8 +371,8 @@ const VipListRecordGrid = ({
         submitText="Save"
         disablePlayerId
       />
-    </Fragment>)
+    </Fragment>
   );
-}
+};
 
 export default VipListRecordGrid;
