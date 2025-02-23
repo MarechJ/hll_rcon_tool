8a4233a2f52963435730e2c035da9774a0e42843
C. Eric Mathey <emathey@protonmail.com>
2025-02-23 12:47:52 -0700

    Register admin accounts associated w/ Player IDs on startup; only register active accounts

--- rconweb/api/apps.py
+++ rconweb/api/apps.py
@@ -1,6 +1,18 @@
 from django.apps import AppConfig
+from rcon.cache_utils import invalidates
 
 
-# TODO: this isn't used anywhere
 class ApiConfig(AppConfig):
     name = "api"
+
+    def ready(self):
+        from rcon.audit import set_registered_mods
+
+        # Can't import from rconweb.api until Django is ready
+        from .auth import get_moderators_accounts
+
+        # Invalidate the cache on start up because you can modify Django
+        # records while CRCON is offline (through the CLI, etc.)
+        with invalidates(get_moderators_accounts):
+            # Register active admin accounts on startup for the ingame/online mods feature
+            set_registered_mods(get_moderators_accounts())
