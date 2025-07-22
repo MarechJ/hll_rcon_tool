0af2105a009df5084a365c74395ca8a50937a0d6
Dorfieeee <rosendorf.dev@gmail.com>
2025-07-22 15:02:23 +0100

    feat: vm add based on flags, offensive handling, helper texts, remove reminder on vm selection reset

--- rcon/user_config/vote_map.py
+++ rcon/user_config/vote_map.py
@@ -1,8 +1,6 @@
 import enum
 from typing import TypedDict
-
-from pydantic import Field, constr, BaseModel
-
+from pydantic import Field, BaseModel
 from rcon.user_config.utils import BaseUserConfig, key_check, set_user_config
 
 
@@ -21,12 +19,14 @@ class VoteMapType(TypedDict):
     allow_consecutive_skirmishes: bool
     allow_default_to_skirmish: bool
     instruction_text: str
-    thank_you_text: str | None
+    thank_you_text: str
     no_vote_text: str
     reminder_frequency_minutes: int
     allow_opt_out: bool
-    help_text: str | None
+    help_text: str
     vote_flags: list["VoteFlag"]
+    player_choice_flags: list[str]
+    player_choice_help_text: str
 
 
 # Has to inherit from str to allow JSON serialization when
@@ -57,17 +57,33 @@ To never see this message again type in the chat !votemap never
 To renable type: !votemap allow"""
 THANK_YOU_TEXT = "Thanks {player_name}, vote registered for:\n{map_name}"
 NO_VOTE_TEXT = "No votes recorded yet"
-HELP_TEXT = """To vote you must type in the chat (press K to open the chat) !votemap followed by the number of the map you want (from 0 to N), you must write the number without the brackets, e.g.: !votemap 0
+HELP_TEXT = """To vote you must type in the chat (press K to open the chat) !votemap followed by the number of the map you want (from 0 to N), you must write the number without the brackets
+e.g. > !votemap 0
 
-The map numbers appear in the reminder message you get once in a while or if you type !votemap without a number.
+To display the maps and their numbers
+> !votemap
 
-If you want to opt-out of the votemap reminder FOREVER type !votemap never
+To add map to the selection
+> !votemap add
 
-To opt back in again type !votemap allow
+If you want to opt-out of the votemap reminder FOREVER
+> !votemap never
 
-To see the select type !votemap
+To opt back in
+> !votemap allow
 
-To see this message again type !votemap help"""
+To see this message again
+> !votemap help"""
+
+PLAYER_CHOICE_HELP_TEXT="""How to add your map?
+
+Type in the chat:
+> !vm add <map_tag> [game_mode] [attackers | only if game_mode=offensive] [environment]
+e.g. > !vm add car offensive axis day
+
+You can omit some [values].
+Defaults: game_mode=warfare, environment=day, attackers=allies
+e.g. > !vm add car -> Carentan Warfare"""
 
 
 class VoteMapUserConfig(BaseUserConfig):
@@ -87,13 +103,14 @@ class VoteMapUserConfig(BaseUserConfig):
     allow_consecutive_skirmishes: bool = Field(default=False)
     allow_default_to_skirmish: bool = Field(default=False)
     instruction_text: str = Field(default=INSTRUCTION_TEXT)
-    thank_you_text: str | None = Field(default=THANK_YOU_TEXT)
+    thank_you_text: str = Field(default=THANK_YOU_TEXT)
     no_vote_text: str = Field(default=NO_VOTE_TEXT)
     reminder_frequency_minutes: int = Field(ge=0, default=20)
     allow_opt_out: bool = Field(default=True)
-    help_text: str | None = Field(default="") # TODO why is this possibly None?
+    help_text: str = Field(default=HELP_TEXT)
     vote_flags: list[VoteFlag] = Field(default_factory=list, title="Vote Flags", description="Players with a listed flag have their vote counted n times (use lowest value if multiple flags; n ≥ 0).")
-
+    player_choice_flags: list[str] = Field(default_factory=list, title="Player Choice Flags", description="Players having one of these flags are allowed to run `!vm add` commands. When no flags provided, everyone can run it.")
+    player_choice_help_text: str = Field(default=PLAYER_CHOICE_HELP_TEXT)
 
     @staticmethod
     def save_to_db(values: VoteMapType, dry_run=False):
@@ -130,6 +147,8 @@ class VoteMapUserConfig(BaseUserConfig):
             allow_opt_out=values.get("allow_opt_out"),
             help_text=values.get("help_text"),
             vote_flags=values.get("vote_flags"),
+            player_choice_flags=values.get("player_choice_flags"),
+            player_choice_help_text=values.get("player_choice_help_text"),
         )
 
         if not dry_run:
