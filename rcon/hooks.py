a53d938fa0c222f50f1777e5055937baf8029931
Dorfieeee <rosendorf.dev@gmail.com>
2025-07-24 23:37:48 +0100

    removed singleton pattern, parametrized init and several methods

--- rcon/hooks.py
+++ rcon/hooks.py
@@ -75,7 +75,7 @@ ARG_RE = re.compile(r"\$(\d+)")
 
 @on_chat
 def count_map_vote(_, struct_log: StructuredLogLineWithMetaData):
-    VoteMap.instance().handle_vote_command(struct_log)
+    VoteMap().handle_vote_command(struct_log)
 
 
 @on_chat
@@ -249,7 +249,7 @@ def chat_help_command(rcon: Rcon, command: BaseChatCommand, ctx: dict[str, str])
 @on_match_end
 def remind_vote_map(_, struct_log):
     logger.info("Match ended reminding to vote map. %s", struct_log)
-    vote_map = VoteMap.instance()
+    vote_map = VoteMap()
     vote_map.apply_with_retry()
     vote_map.send_reminder(force=True)
 
@@ -325,7 +325,7 @@ def handle_new_match_start(rcon: Rcon, struct_log):
         raise
     finally:
         logger.info("New match started initializing vote map. %s", struct_log)
-        VoteMap.instance().restart()
+        VoteMap().restart()
         try:
             record_stats_worker(MapsHistory()[1])
         except Exception:
