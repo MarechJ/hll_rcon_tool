10c01d6c3bf1f9a01b66bb3997783be855b84d57
Dorfieeee <rosendorf.dev@gmail.com>
2026-08-01 11:39:27 +0100

    log recorded - log saved logs

--- rcon/logs/recorder.py
+++ rcon/logs/recorder.py
@@ -88,6 +88,7 @@ class LogRecorder:
             if log["player_id_2"]:
                 player_2 = players[log["player_id_2"]]
 
+            logger.info("Saving log: [%d] -> %s", log["timestamp_ms"], log["raw"])
             rows.append(
                 {
                     "version": log["version"],
@@ -103,6 +104,7 @@ class LogRecorder:
                     "weapon": log["weapon"],
                 }
             )
+
 
         try:
             if sess.get_bind().dialect.name == "postgresql":
